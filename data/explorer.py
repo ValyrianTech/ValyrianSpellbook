2042ca37c57522d2df2c92099e47853c423528c8
Wouter Glorieux <wouter@valyrian.tech>
2017-09-15 18:41:24 +0200

    added explorer class

---
+++ data/explorer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class ExplorerType(object):
+    BLOCKCHAIN_INFO = 'Blockchain.info'
+    BLOCKTRAIL_COM = 'Blocktrail.com'
+    INSIGHT = 'Insight'
+    BITCOIN_CORE = 'Bitcoin Core'
+    GOCOIN = 'GoCoin'
+
+
+class Explorer(object):
+    def __init__(self):
+        self.api_key = ''
+        self.url = ''
+        self.explorer_type = None
+        self.priority = 0
+
+    def json_encodable(self):
+        return {'type': self.explorer_type,
+                'priority': self.priority,
+                'url': self.url,
+                'api_key': self.api_key}
+
