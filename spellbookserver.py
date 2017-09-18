dc29c50d4388ce5575a4f3667235384a4314a695
Wouter Glorieux <wouter@valyrian.tech>
2017-09-19 00:02:35 +0200

    removed redundant space

--- spellbookserver.py
+++ spellbookserver.py
@@ -38,7 +38,7 @@ class SpellbookRESTAPI(Bottle):
         self.install(self.log_to_logger)
 
         # Make sure that an api_keys.json file is present, the first time the server is started
-        #  a new random api key and secret pair will be generated
+        # a new random api key and secret pair will be generated
         if not os.path.isfile('api_keys.json'):
             self.log.info('Generating new API keys')
             initialize_api_keys_file()
