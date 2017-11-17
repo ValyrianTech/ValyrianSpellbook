b663128ce87ef074e06f1ab16887111e8357783e
Wouter Glorieux <wouter@valyrian.tech>
2017-11-17 19:17:58 +0100

    removed parameter explorer from linker module

--- linker/linker.py
+++ linker/linker.py
@@ -7,14 +7,14 @@ from BIP44.BIP44 import get_addresses_from_xpub
 from validators.validators import valid_address, valid_xpub
 
 
-def get_lal(address, xpub, block_height=0, explorer=None):
+def get_lal(address, xpub, block_height=0):
     if not valid_address(address):
         return {'error': 'Invalid address: %s' % address}
 
     if not valid_xpub(xpub):
         return {'error': 'Invalid xpub: %s' % xpub}
 
-    sil_data = get_sil(address, block_height, explorer)
+    sil_data = get_sil(address, block_height)
 
     if 'SIL' in sil_data and 'explorer' in sil_data:
         sil = sil_data['SIL']
@@ -30,8 +30,8 @@ def get_lal(address, xpub, block_height=0, explorer=None):
         return {'error': 'Received invalid SIL data: %s' % sil_data}
 
 
-def get_lbl(address, xpub, block_height=0, explorer=None):
-    lal_data = get_lal(address, xpub, block_height, explorer)
+def get_lbl(address, xpub, block_height=0):
+    lal_data = get_lal(address, xpub, block_height)
 
     if 'error' in lal_data:
         return lal_data['error']
@@ -42,7 +42,7 @@ def get_lbl(address, xpub, block_height=0, explorer=None):
     lbl = []
     for i in range(0, len(lal)):
         address = lal[i][0]
-        linked_balance = balance(lal[i][1], explorer)
+        linked_balance = balance(lal[i][1])
         if 'balance' in linked_balance and 'final' in linked_balance['balance'] and 'explorer' in linked_balance:
             lbl.append([address, linked_balance['balance']['final']])
             last_explorer = linked_balance['explorer']
@@ -57,8 +57,8 @@ def get_lbl(address, xpub, block_height=0, explorer=None):
             'explorer': last_explorer}
 
 
-def get_lrl(address, xpub, block_height=0, explorer=None):
-    lal_data = get_lal(address, xpub, block_height, explorer)
+def get_lrl(address, xpub, block_height=0):
+    lal_data = get_lal(address, xpub, block_height)
 
     if 'error' in lal_data:
         return lal_data['error']
@@ -69,7 +69,7 @@ def get_lrl(address, xpub, block_height=0, explorer=None):
     lrl = []
     for i in range(0, len(lal)):
         address = lal[i][0]
-        linked_balance = balance(lal[i][1], explorer)
+        linked_balance = balance(lal[i][1])
         if 'balance' in linked_balance and 'received' in linked_balance['balance'] and 'explorer' in linked_balance:
             lrl.append([address, linked_balance['balance']['received']])
             last_explorer = linked_balance['explorer']
@@ -84,8 +84,8 @@ def get_lrl(address, xpub, block_height=0, explorer=None):
             'explorer': last_explorer}
 
 
-def get_lsl(address, xpub, block_height=0, explorer=None):
-    lal_data = get_lal(address, xpub, block_height, explorer)
+def get_lsl(address, xpub, block_height=0):
+    lal_data = get_lal(address, xpub, block_height)
 
     if 'error' in lal_data:
         return lal_data['error']
@@ -96,7 +96,7 @@ def get_lsl(address, xpub, block_height=0, explorer=None):
     lsl = []
     for i in range(0, len(lal)):
         address = lal[i][0]
-        linked_balance = balance(lal[i][1], explorer)
+        linked_balance = balance(lal[i][1])
         if 'balance' in linked_balance and 'sent' in linked_balance['balance'] and 'explorer' in linked_balance:
             lsl.append([address, linked_balance['balance']['sent']])
             last_explorer = linked_balance['explorer']
