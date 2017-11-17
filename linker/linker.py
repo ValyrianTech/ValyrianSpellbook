31a5e7b8e6623b121cb0e6e560979da4caff46da
Wouter Glorieux <wouter@valyrian.tech>
2017-11-17 20:07:22 +0100

    removed the 'explorer' key from the return value of get_lal, get_lbl, get_lsl, get_lrl

--- linker/linker.py
+++ linker/linker.py
@@ -16,7 +16,7 @@ def get_lal(address, xpub, block_height=0):
 
     sil_data = get_sil(address, block_height)
 
-    if 'SIL' in sil_data and 'explorer' in sil_data:
+    if 'SIL' in sil_data:
         sil = sil_data['SIL']
         linked_addresses = get_addresses_from_xpub(xpub, len(sil))
 
@@ -24,8 +24,7 @@ def get_lal(address, xpub, block_height=0):
         for i in range(0, len(sil)):
             lal.append([sil[i][0], linked_addresses[i]])
 
-        return {'LAL': lal,
-                'explorer': sil_data['explorer']}
+        return {'LAL': lal}
     else:
         return {'error': 'Received invalid SIL data: %s' % sil_data}
 
@@ -37,15 +36,13 @@ def get_lbl(address, xpub, block_height=0):
         return lal_data['error']
 
     lal = lal_data['LAL']
-    last_explorer = lal_data['explorer']
 
     lbl = []
     for i in range(0, len(lal)):
         address = lal[i][0]
         linked_balance = balance(lal[i][1])
-        if 'balance' in linked_balance and 'final' in linked_balance['balance'] and 'explorer' in linked_balance:
+        if 'balance' in linked_balance and 'final' in linked_balance['balance']:
             lbl.append([address, linked_balance['balance']['final']])
-            last_explorer = linked_balance['explorer']
         else:
             return {'error': 'Failed to retrieve balance of %s' % lal[i][1]}
 
@@ -53,8 +50,7 @@ def get_lbl(address, xpub, block_height=0):
     for row in lbl:
         row.append(row[1] / total)
 
-    return {'LBL': lbl,
-            'explorer': last_explorer}
+    return {'LBL': lbl}
 
 
 def get_lrl(address, xpub, block_height=0):
@@ -64,15 +60,13 @@ def get_lrl(address, xpub, block_height=0):
         return lal_data['error']
 
     lal = lal_data['LAL']
-    last_explorer = lal_data['explorer']
 
     lrl = []
     for i in range(0, len(lal)):
         address = lal[i][0]
         linked_balance = balance(lal[i][1])
-        if 'balance' in linked_balance and 'received' in linked_balance['balance'] and 'explorer' in linked_balance:
+        if 'balance' in linked_balance and 'received' in linked_balance['balance']:
             lrl.append([address, linked_balance['balance']['received']])
-            last_explorer = linked_balance['explorer']
         else:
             return {'error': 'Failed to retrieve balance of %s' % lal[i][1]}
 
@@ -80,8 +74,7 @@ def get_lrl(address, xpub, block_height=0):
     for row in lrl:
         row.append(row[1] / total)
 
-    return {'LRL': lrl,
-            'explorer': last_explorer}
+    return {'LRL': lrl}
 
 
 def get_lsl(address, xpub, block_height=0):
@@ -91,15 +84,13 @@ def get_lsl(address, xpub, block_height=0):
         return lal_data['error']
 
     lal = lal_data['LAL']
-    last_explorer = lal_data['explorer']
 
     lsl = []
     for i in range(0, len(lal)):
         address = lal[i][0]
         linked_balance = balance(lal[i][1])
-        if 'balance' in linked_balance and 'sent' in linked_balance['balance'] and 'explorer' in linked_balance:
+        if 'balance' in linked_balance and 'sent' in linked_balance['balance']:
             lsl.append([address, linked_balance['balance']['sent']])
-            last_explorer = linked_balance['explorer']
         else:
             return {'error': 'Failed to retrieve balance of %s' % lal[i][1]}
 
@@ -107,5 +98,4 @@ def get_lsl(address, xpub, block_height=0):
     for row in lsl:
         row.append(row[1] / total)
 
-    return {'LSL': lsl,
-            'explorer': last_explorer}
+    return {'LSL': lsl}
