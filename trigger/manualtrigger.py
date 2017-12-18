597217cb377a658904ea9d3bf4418823e795116f
Wouter Glorieux <wouter@valyrian.tech>
2017-12-18 21:09:46 +0100

    added ManualTrigger

---
+++ trigger/manualtrigger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from trigger import Trigger
+from triggertype import TriggerType
+
+
+class ManualTrigger(Trigger):
+    def __init__(self, trigger_id):
+        super(ManualTrigger, self).__init__(trigger_id=trigger_id)
+        self.trigger_type = TriggerType.MANUAL
+
+    def conditions_fulfilled(self):
+        # Manual triggers can only be triggered manually, so always return False
+        return False
