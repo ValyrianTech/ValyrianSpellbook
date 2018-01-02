868466ec790e290a3188feea782a04dd4f39cbc1
Wouter Glorieux <wouter@valyrian.tech>
2018-01-02 15:49:10 +0100

    added enum class ActionType

---
+++ action/actiontype.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class ActionType(object):
+    COMMAND = 'Command'
+    DISTRIBUTER = 'Distributer'
+    FORWARDER = 'Forwarder'
+    OPRETURNWRITER = 'OpReturnWriter'
+    REVEALLINK = 'RevealLink'
+    REVEALTEXT = 'RevealText'
+    SENDMAIL = 'SendMail'
+    WEBHOOK = 'WebhookAction'
