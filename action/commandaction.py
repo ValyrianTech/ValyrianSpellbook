4f624b9c58d1ef9b7de7585bc3abb64e80903a5b
Wouter Glorieux <wouter@valyrian.tech>
2018-01-02 15:47:50 +0100

    added child class CommandAction

---
+++ action/commandaction.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+from subprocess import Popen, PIPE
+
+from action import Action
+from actiontype import ActionType
+
+
+class CommandAction(Action):
+    def __init__(self, action_id):
+        super(CommandAction, self).__init__(action_id=action_id)
+        self.action_type = ActionType.COMMAND
+
+    def run(self):
+        if self.run_command is None or self.run_command == '':
+            return False
+
+        command_args = [str(arg) for arg in self.run_command.split()]
+
+        logging.getLogger('Spellbook').info('Running command: %s' % ' '.join(command_args))
+        command_process = Popen(command_args, stdout=PIPE, stderr=PIPE, shell=True)
+        output, error = command_process.communicate()
+        stripped_output = output.strip()
+        logging.getLogger('Spellbook').info('Command output: %s' % stripped_output)
+
+        stripped_error = error.strip()
+        if len(stripped_error):
+            logging.getLogger('Spellbook').error('Command error: %s' % stripped_error)
+
+        return True if command_process.returncode == 0 else False
