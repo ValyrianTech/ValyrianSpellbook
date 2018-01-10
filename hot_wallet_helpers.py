432ff11f4a6367d9ffb6b5e57e5b520279e1f729
Wouter Glorieux <wouter@valyrian.tech>
2018-01-10 20:43:06 +0100

    added some helper functions for the hot wallet

---
+++ hot_wallet_helpers.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import getpass
+import simplejson
+from AESCipher import AESCipher
+from configurationhelpers import get_wallet_info
+
+HOT_WALLET_PASSWORD = None
+
+
+def get_hot_wallet():
+    global HOT_WALLET_PASSWORD
+
+    if HOT_WALLET_PASSWORD is None:
+        prompt_decryption_password()
+
+    wallet_dir, wallet_id = get_wallet_info()
+
+    try:
+        cipher = AESCipher(key=HOT_WALLET_PASSWORD)
+        with open(os.path.join(wallet_dir, '%s.enc' % wallet_id), 'r') as input_file:
+            encrypted_data = input_file.read()
+            return simplejson.loads(cipher.decrypt(encrypted_data))
+
+    except Exception:
+        raise Exception('Invalid password to decrypt hot wallet!')
+
+
+def prompt_decryption_password():
+    global HOT_WALLET_PASSWORD
+    # if this is running in pycharm console, make sure 'Emulate terminal in output console' is checked in the configuration
+    HOT_WALLET_PASSWORD = getpass.getpass('Enter the password to decrypt the hot wallet: ')
+
