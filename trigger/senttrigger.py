e3d608cbef000bf2206341b2cb3429f108dcd10c
Wouter Glorieux <wouter@valyrian.tech>
2018-02-12 22:10:08 +0100

    moved SentTrigger specific variables to the right subclass

--- trigger/senttrigger.py
+++ trigger/senttrigger.py
@@ -4,12 +4,15 @@
 from trigger import Trigger
 from triggertype import TriggerType
 from data.data import balance
+from validators.validators import valid_address, valid_amount
 
 
 class SentTrigger(Trigger):
     def __init__(self, trigger_id):
         super(SentTrigger, self).__init__(trigger_id=trigger_id)
         self.trigger_type = TriggerType.SENT
+        self.address = None
+        self.amount = None
 
     def conditions_fulfilled(self):
         if self.address is None or self.amount is None:
@@ -23,3 +26,19 @@ class SentTrigger(Trigger):
             return False
 
         return True if self.amount <= total_sent else False
+
+    def configure(self, **config):
+        super(SentTrigger, self).configure(**config)
+        if 'address' in config and valid_address(config['address']):
+            self.address = config['address']
+
+        if 'amount' in config and valid_amount(config['amount']):
+            self.amount = config['amount']
+
+    def json_encodable(self):
+        ret = super(SentTrigger, self).json_encodable()
+
+        ret.update({
+            'address': self.address,
+            'amount': self.amount})
+        return ret
