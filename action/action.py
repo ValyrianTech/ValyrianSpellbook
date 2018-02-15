b1749450086af962a0bf990244d63acd3306f452
Wouter Glorieux <wouter@valyrian.tech>
2018-02-15 17:43:52 +0100

    moved SendTransactionAction specific variables to the right subclass

--- action/action.py
+++ action/action.py
@@ -7,13 +7,7 @@ from abc import abstractmethod, ABCMeta
 from datetime import datetime
 
 from jsonhelpers import save_to_json_file
-from validators.validators import valid_action_type, valid_address, valid_percentage, valid_xpub, valid_amount, valid_op_return, valid_block_height
-from validators.validators import valid_transaction_type, valid_distribution
-from hot_wallet_helpers import get_address_from_wallet
-from BIP44.BIP44 import set_testnet
-from transactiontype import TransactionType
-from configurationhelpers import get_minimum_output_value, get_use_testnet
-
+from validators.validators import valid_action_type
 
 ACTIONS_DIR = 'json/public/actions'
 
@@ -25,25 +19,6 @@ class Action(object):
         self.id = action_id
         self.action_type = None
         self.created = None
-        self.fee_address = None
-        self.fee_percentage = 0
-        self.fee_minimum_amount = 1000
-        self.wallet_type = None
-        self.sending_address = None
-        self.bip44_account = None
-        self.bip44_index = None
-        self.receiving_address = None
-        self.receiving_xpub = None
-        self.amount = 0
-        self.minimum_amount = None
-        self.op_return_data = None
-        self.change_address = None
-        self.registration_address = None
-        self.registration_block_height = None
-        self.registration_xpub = None
-        self.distribution = None
-        self.transaction_type = TransactionType.SEND2SINGLE
-        self.minimum_output_value = get_minimum_output_value()
 
     def configure(self, **config):
         self.created = datetime.fromtimestamp(config['created']) if 'created' in config else datetime.now()
@@ -51,92 +26,13 @@ class Action(object):
         if 'action_type' in config and valid_action_type(config['action_type']):
             self.action_type = config['action_type']
 
-        if 'fee_address' in config and valid_address(config['fee_address']):
-            self.fee_address = config['fee_address']
-
-        if 'fee_percentage' in config and valid_percentage(config['fee_percentage']):
-            self.fee_percentage = config['fee_percentage']
-
-        if 'wallet_type' in config and config['wallet_type'] in ['Single', 'BIP44']:
-            self.wallet_type = config['wallet_type']
-
-        if 'sending_address' in config and valid_address(config['sending_address']):
-            self.sending_address = config['sending_address']
-
-        if 'bip44_account' in config:
-            self.bip44_account = config['bip44_account']
-
-        if 'bip44_index' in config:
-            self.bip44_index = config['bip44_index']
-
-        if 'receiving_address' in config and valid_address(config['receiving_address']):
-            self.receiving_address = config['receiving_address']
-
-        if 'receiving_xpub' in config and valid_xpub(config['receiving_xpub']):
-            self.receiving_xpub = config['receiving_xpub']
-
-        if 'amount' in config and valid_amount(config['amount']):
-            self.amount = config['amount']
-
-        if 'minimum_amount' in config and valid_amount(config['minimum_amount']):
-            self.minimum_amount = config['minimum_amount']
-
-        if 'op_return_data' in config and valid_op_return(config['op_return_data']):
-            self.op_return_data = config['op_return_data']
-
-        if 'change_address' in config and valid_address(config['change_address']):
-            self.receiving_address = config['change_address']
-
-        if 'transaction_type' in config and valid_transaction_type(config['transaction_type']):
-            self.transaction_type = config['transaction_type']
-
-        if 'minimum_output_value' in config and valid_amount(config['minimum_output_value']):
-            self.minimum_output_value = config['minimum_output_value']
-
-        if 'registration_address' in config and valid_address(config['registration_address']):
-            self.registration_address = config['registration_address']
-
-        if 'registration_block_height' in config and valid_block_height(config['registration_block_height']):
-            self.registration_block_height = config['registration_block_height']
-
-        if 'registration_xpub' in config and valid_xpub(config['registration_xpub']):
-            self.registration_xpub = config['registration_xpub']
-
-        if 'distribution' in config and valid_distribution(config['distribution']):
-            self.distribution = config['distribution']
-
-        # fill in the address in case of a BIP44 hot wallet
-        if self.wallet_type == 'BIP44':
-            # Set BIP44 module to use testnet if necessary, configured in the spellbook.conf file under [Wallet] -> use_testnet
-            set_testnet(get_use_testnet())
-
-            self.sending_address = get_address_from_wallet(self.bip44_account, self.bip44_index)
-
     def save(self):
         save_to_json_file(os.path.join(ACTIONS_DIR, '%s.json' % self.id), self.json_encodable())
 
     def json_encodable(self):
         return {'id': self.id,
                 'action_type': self.action_type,
-                'created': int(time.mktime(self.created.timetuple())),
-                'fee_address': self.fee_address,
-                'fee_percentage': self.fee_percentage,
-                'wallet_type': self.wallet_type,
-                'sending_address': self.sending_address,
-                'bip44_account': self.bip44_account,
-                'bip44_index': self.bip44_index,
-                'receiving_address': self.receiving_address,
-                'receiving_xpub': self.receiving_xpub,
-                'amount': self.amount,
-                'minimum_amount': self.minimum_amount,
-                'op_return_data': self.op_return_data,
-                'change_address': self.change_address,
-                'transaction_type': self.transaction_type,
-                'minimum_output_value': self.minimum_output_value,
-                'registration_address': self.registration_address,
-                'registration_block_height': self.registration_block_height,
-                'registration_xpub': self.registration_xpub,
-                'distribution': self.distribution}
+                'created': int(time.mktime(self.created.timetuple()))}
 
     @abstractmethod
     def run(self):
--- action/sendtransactionaction.py
+++ action/sendtransactionaction.py
@@ -14,13 +14,122 @@ from hot_wallet_helpers import get_hot_wallet
 from BIP44.BIP44 import get_xpriv_key, get_private_key, set_testnet
 from transactionfactory import make_custom_tx
 from configurationhelpers import get_max_tx_fee_percentage, get_use_testnet
+from transactiontype import TransactionType
+from configurationhelpers import get_minimum_output_value
+from validators.validators import valid_address, valid_xpub, valid_amount, valid_op_return, valid_block_height
+from validators.validators import valid_transaction_type, valid_distribution, valid_percentage
+from hot_wallet_helpers import get_address_from_wallet
 
 
 class SendTransactionAction(Action):
     def __init__(self, action_id):
         super(SendTransactionAction, self).__init__(action_id=action_id)
         self.action_type = ActionType.SENDTRANSACTION
+        self.fee_address = None
+        self.fee_percentage = 0
+        self.fee_minimum_amount = 1000
+        self.wallet_type = None
+        self.sending_address = None
+        self.bip44_account = None
+        self.bip44_index = None
+        self.receiving_address = None
+        self.receiving_xpub = None
+        self.amount = 0
+        self.minimum_amount = None
+        self.op_return_data = None
+        self.change_address = None
+        self.registration_address = None
+        self.registration_block_height = None
+        self.registration_xpub = None
+        self.distribution = None
+        self.transaction_type = TransactionType.SEND2SINGLE
+        self.minimum_output_value = get_minimum_output_value()
         self.unspent_outputs = None
+
+    def configure(self, **config):
+        super(SendTransactionAction, self).configure(**config)
+        if 'fee_address' in config and valid_address(config['fee_address']):
+            self.fee_address = config['fee_address']
+
+        if 'fee_percentage' in config and valid_percentage(config['fee_percentage']):
+            self.fee_percentage = config['fee_percentage']
+
+        if 'wallet_type' in config and config['wallet_type'] in ['Single', 'BIP44']:
+            self.wallet_type = config['wallet_type']
+
+        if 'sending_address' in config and valid_address(config['sending_address']):
+            self.sending_address = config['sending_address']
+
+        if 'bip44_account' in config:
+            self.bip44_account = config['bip44_account']
+
+        if 'bip44_index' in config:
+            self.bip44_index = config['bip44_index']
+
+        if 'receiving_address' in config and valid_address(config['receiving_address']):
+            self.receiving_address = config['receiving_address']
+
+        if 'receiving_xpub' in config and valid_xpub(config['receiving_xpub']):
+            self.receiving_xpub = config['receiving_xpub']
+
+        if 'amount' in config and valid_amount(config['amount']):
+            self.amount = config['amount']
+
+        if 'minimum_amount' in config and valid_amount(config['minimum_amount']):
+            self.minimum_amount = config['minimum_amount']
+
+        if 'op_return_data' in config and valid_op_return(config['op_return_data']):
+            self.op_return_data = config['op_return_data']
+
+        if 'change_address' in config and valid_address(config['change_address']):
+            self.receiving_address = config['change_address']
+
+        if 'transaction_type' in config and valid_transaction_type(config['transaction_type']):
+            self.transaction_type = config['transaction_type']
+
+        if 'minimum_output_value' in config and valid_amount(config['minimum_output_value']):
+            self.minimum_output_value = config['minimum_output_value']
+
+        if 'registration_address' in config and valid_address(config['registration_address']):
+            self.registration_address = config['registration_address']
+
+        if 'registration_block_height' in config and valid_block_height(config['registration_block_height']):
+            self.registration_block_height = config['registration_block_height']
+
+        if 'registration_xpub' in config and valid_xpub(config['registration_xpub']):
+            self.registration_xpub = config['registration_xpub']
+
+        if 'distribution' in config and valid_distribution(config['distribution']):
+            self.distribution = config['distribution']
+
+        # fill in the address in case of a BIP44 hot wallet
+        if self.wallet_type == 'BIP44':
+            # Set BIP44 module to use testnet if necessary, configured in the spellbook.conf file under [Wallet] -> use_testnet
+            set_testnet(get_use_testnet())
+
+            self.sending_address = get_address_from_wallet(self.bip44_account, self.bip44_index)
+
+    def json_encodable(self):
+        ret = super(SendTransactionAction, self).json_encodable()
+        ret.update({'fee_address': self.fee_address,
+                    'fee_percentage': self.fee_percentage,
+                    'wallet_type': self.wallet_type,
+                    'sending_address': self.sending_address,
+                    'bip44_account': self.bip44_account,
+                    'bip44_index': self.bip44_index,
+                    'receiving_address': self.receiving_address,
+                    'receiving_xpub': self.receiving_xpub,
+                    'amount': self.amount,
+                    'minimum_amount': self.minimum_amount,
+                    'op_return_data': self.op_return_data,
+                    'change_address': self.change_address,
+                    'transaction_type': self.transaction_type,
+                    'minimum_output_value': self.minimum_output_value,
+                    'registration_address': self.registration_address,
+                    'registration_block_height': self.registration_block_height,
+                    'registration_xpub': self.registration_xpub,
+                    'distribution': self.distribution})
+        return ret
 
     def run(self):
         if self.sending_address is None:
