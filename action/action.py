4b93f123523855cf1d0703414c4913f34db09fae
Wouter Glorieux <wouter@valyrian.tech>
2018-02-15 17:04:06 +0100

    moved CommandAction specific variables to the right subclass added integrationtest_action_Command

--- action/action.py
+++ action/action.py
@@ -25,7 +25,6 @@ class Action(object):
         self.id = action_id
         self.action_type = None
         self.created = None
-        self.run_command = None
         self.mail_recipients = None
         self.mail_subject = None
         self.mail_body_template = None
@@ -57,9 +56,6 @@ class Action(object):
 
         if 'action_type' in config and valid_action_type(config['action_type']):
             self.action_type = config['action_type']
-
-        if 'run_command' in config:
-            self.run_command = config['run_command']
 
         if 'mail_recipients' in config:
             self.mail_recipients = config['mail_recipients']
@@ -147,7 +143,6 @@ class Action(object):
         return {'id': self.id,
                 'action_type': self.action_type,
                 'created': int(time.mktime(self.created.timetuple())),
-                'run_command': self.run_command,
                 'mail_recipients': self.mail_recipients,
                 'mail_subject': self.mail_subject,
                 'mail_body_template': self.mail_body_template,
