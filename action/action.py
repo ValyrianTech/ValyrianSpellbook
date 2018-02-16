a9d5fdd49823d5bb8a1905700bbe887a98c6bc79
Wouter Glorieux <wouter@valyrian.tech>
2018-02-16 15:49:41 +0100

    added docstrings

--- action/action.py
+++ action/action.py
@@ -16,20 +16,41 @@ class Action(object):
     __metaclass__ = ABCMeta
 
     def __init__(self, action_id):
+        """
+        Constructor for an Action object
+        This is an abstract class of which the different action types will be derived
+
+        :param action_id: An id for the action
+        """
         self.id = action_id
         self.action_type = None
         self.created = None
 
     def configure(self, **config):
+        """
+        Configure the action with given config settings
+
+        :param config: A dict containing the configuration settings
+                       - config['created']     : A timestamp when the action was created
+                       - config['action_type'] : The type of action ['Command', 'RevealSecret', 'SendMail', 'SendTransaction', 'Webhook']
+        """
         self.created = datetime.fromtimestamp(config['created']) if 'created' in config else datetime.now()
 
         if 'action_type' in config and valid_action_type(config['action_type']):
             self.action_type = config['action_type']
 
     def save(self):
+        """
+        Save the action as a json file
+        """
         save_to_json_file(os.path.join(ACTIONS_DIR, '%s.json' % self.id), self.json_encodable())
 
     def json_encodable(self):
+        """
+        Get the action config in a json encodable format
+
+        :return: A dict containing the configuration settings
+        """
         return {'id': self.id,
                 'action_type': self.action_type,
                 'created': int(time.mktime(self.created.timetuple()))}
