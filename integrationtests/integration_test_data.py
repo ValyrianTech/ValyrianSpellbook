ce6ba89b4a08241a706b26a454664cb6e5d6e1f1
Wouter Glorieux <wouter@valyrian.tech>
2018-02-27 18:43:19 +0100

    make integrationtest work with testnet

--- integrationtests/integration_test_data.py
+++ integrationtests/integration_test_data.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 
 from integration_test_helpers import spellbook_call
+from helpers.BIP44 import set_testnet
+from helpers.configurationhelpers import get_use_testnet
+from helpers.hotwallethelpers import get_address_from_wallet, get_xpub_key_from_wallet
 
+# Change working dir up one level
+os.chdir("..")
+
+set_testnet(get_use_testnet())
 
 print 'Starting Spellbook integration test: data'
 print '----------------------------------------------\n'
@@ -25,68 +33,75 @@ if not (blockchain_info['block']['time'] == blocktrail_com['block']['time'] == b
 
 
 # --------------------------------------------------------------------------------------------------------
-print 'Getting block 488470 from Blockchain.info'
-blockchain_info = spellbook_call('get_block', 488470, '-e=blockchain.info')
+block_height = 488470
+print 'Getting block %s from Blockchain.info' % block_height
+blockchain_info = spellbook_call('get_block', block_height, '-e=blockchain.info')
 
-print 'Getting block 488470 from Blocktrail.com'
-blocktrail_com = spellbook_call('get_block', 488470, '-e=blocktrail.com')
+print 'Getting block %s from Blocktrail.com' % block_height
+blocktrail_com = spellbook_call('get_block', block_height, '-e=blocktrail.com')
 
-print 'Getting block 488470 from Blockexplorer.com'
-blockexplorer_com = spellbook_call('get_block', 488470, '-e=blockexplorer.com')
-
-assert blockchain_info['block']['hash'] == blocktrail_com['block']['hash'] == blockexplorer_com['block']['hash']
-assert blockchain_info['block']['height'] == blocktrail_com['block']['height'] == blockexplorer_com['block']['height']
-assert blockchain_info['block']['time'] == blocktrail_com['block']['time'] == blockexplorer_com['block']['time']
-
-# --------------------------------------------------------------------------------------------------------
-print 'Getting block 000000000000000000f6af507822a695390bada30cbd0c517c12442effb277af from Blockchain.info'
-blockchain_info = spellbook_call('get_block', '000000000000000000f6af507822a695390bada30cbd0c517c12442effb277af', '-e=blockchain.info')
-
-print 'Getting block 000000000000000000f6af507822a695390bada30cbd0c517c12442effb277af from Blocktrail.com'
-blocktrail_com = spellbook_call('get_block', '000000000000000000f6af507822a695390bada30cbd0c517c12442effb277af', '-e=blocktrail.com')
-
-print 'Getting block 000000000000000000f6af507822a695390bada30cbd0c517c12442effb277af from Blockexplorer.com'
-blockexplorer_com = spellbook_call('get_block', '000000000000000000f6af507822a695390bada30cbd0c517c12442effb277af', '-e=blockexplorer.com')
+print 'Getting block %s from Blockexplorer.com' % block_height
+blockexplorer_com = spellbook_call('get_block', block_height, '-e=blockexplorer.com')
 
 assert blockchain_info['block']['hash'] == blocktrail_com['block']['hash'] == blockexplorer_com['block']['hash']
 assert blockchain_info['block']['height'] == blocktrail_com['block']['height'] == blockexplorer_com['block']['height']
 assert blockchain_info['block']['time'] == blocktrail_com['block']['time'] == blockexplorer_com['block']['time']
 
+block_hash = blockchain_info['block']['hash']
+
 # --------------------------------------------------------------------------------------------------------
-print 'Getting prime input address of tx 39bb5f5d50882227f93b980df15ea676414f0363770a0174a13c8f55c877b598 from Blockchain.info'
-blockchain_info = spellbook_call('get_prime_input_address', '39bb5f5d50882227f93b980df15ea676414f0363770a0174a13c8f55c877b598', '-e=blockchain.info')
+print 'Getting block %s from Blockchain.info' % block_hash
+blockchain_info = spellbook_call('get_block', block_hash, '-e=blockchain.info')
 
-print 'Getting prime input address of tx 39bb5f5d50882227f93b980df15ea676414f0363770a0174a13c8f55c877b598 from Blocktrail.com'
-blocktrail_com = spellbook_call('get_prime_input_address', '39bb5f5d50882227f93b980df15ea676414f0363770a0174a13c8f55c877b598', '-e=blocktrail.com')
+print 'Getting block %s from Blocktrail.com' % block_hash
+blocktrail_com = spellbook_call('get_block', block_hash, '-e=blocktrail.com')
 
-print 'Getting prime input address of tx 39bb5f5d50882227f93b980df15ea676414f0363770a0174a13c8f55c877b598 from Blockexplorer.com'
-blockexplorer_com = spellbook_call('get_prime_input_address', '39bb5f5d50882227f93b980df15ea676414f0363770a0174a13c8f55c877b598', '-e=blockexplorer.com')
+print 'Getting block %s from Blockexplorer.com' % block_hash
+blockexplorer_com = spellbook_call('get_block', block_hash, '-e=blockexplorer.com')
+
+assert blockchain_info['block']['hash'] == blocktrail_com['block']['hash'] == blockexplorer_com['block']['hash']
+assert blockchain_info['block']['height'] == blocktrail_com['block']['height'] == blockexplorer_com['block']['height']
+assert blockchain_info['block']['time'] == blocktrail_com['block']['time'] == blockexplorer_com['block']['time']
+
+
+
+# --------------------------------------------------------------------------------------------------------
+address = get_address_from_wallet(1, 0)  # use the first address of the second account in the hot wallet
+print 'Getting transactions of address %s from Blockchain.info' % address
+blockchain_info = spellbook_call('get_transactions', address, '-e=blockchain.info')
+
+print 'Getting transactions of address %s from Blocktrail.com' % address
+blocktrail_com = spellbook_call('get_transactions', address, '-e=blocktrail.com')
+
+print 'Getting transactions of address %s from Blockexplorer.com' % address
+blockexplorer_com = spellbook_call('get_transactions', address, '-e=blockexplorer.com')
+
+assert len(blockchain_info['transactions']) == len(blocktrail_com['transactions']) == len(blockexplorer_com['transactions'])
+
+txid = blockchain_info['transactions'][0]['txid']
+# --------------------------------------------------------------------------------------------------------
+
+print 'Getting prime input address of tx %s from Blockchain.info' % txid
+blockchain_info = spellbook_call('get_prime_input_address', txid, '-e=blockchain.info')
+
+print 'Getting prime input address of tx %s from Blocktrail.com' % txid
+blocktrail_com = spellbook_call('get_prime_input_address', txid, '-e=blocktrail.com')
+
+print 'Getting prime input address of tx %s from Blockexplorer.com' % txid
+blockexplorer_com = spellbook_call('get_prime_input_address', txid, '-e=blockexplorer.com')
 
 assert blockchain_info['prime_input_address'] == blocktrail_com['prime_input_address'] == blockexplorer_com['prime_input_address']
 
 
 # --------------------------------------------------------------------------------------------------------
-print 'Getting transactions of address 1Robbk6PuJst6ot6ay2DcVugv8nxfJh5y from Blockchain.info'
-blockchain_info = spellbook_call('get_transactions', '1Robbk6PuJst6ot6ay2DcVugv8nxfJh5y', '-e=blockchain.info')
+print 'Getting balance of address %s from Blockchain.info' % address
+blockchain_info = spellbook_call('get_balance', address, '-e=blockchain.info')
 
-print 'Getting transactions of address 1Robbk6PuJst6ot6ay2DcVugv8nxfJh5y from Blocktrail.com'
-blocktrail_com = spellbook_call('get_transactions', '1Robbk6PuJst6ot6ay2DcVugv8nxfJh5y', '-e=blocktrail.com')
+print 'Getting balance of address %s from Blocktrail.com' % address
+blocktrail_com = spellbook_call('get_balance', address, '-e=blocktrail.com')
 
-print 'Getting transactions of address 1Robbk6PuJst6ot6ay2DcVugv8nxfJh5y from Blockexplorer.com'
-blockexplorer_com = spellbook_call('get_transactions', '1Robbk6PuJst6ot6ay2DcVugv8nxfJh5y', '-e=blockexplorer.com')
-
-assert len(blockchain_info['transactions']) == len(blocktrail_com['transactions']) == len(blockexplorer_com['transactions'])
-
-
-# --------------------------------------------------------------------------------------------------------
-print 'Getting balance of address 1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8 from Blockchain.info'
-blockchain_info = spellbook_call('get_balance', '1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8', '-e=blockchain.info')
-
-print 'Getting balance of address 1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8 from Blocktrail.com'
-blocktrail_com = spellbook_call('get_balance', '1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8', '-e=blocktrail.com')
-
-print 'Getting balance of address 1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8 from Blockexplorer.com'
-blockexplorer_com = spellbook_call('get_balance', '1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8', '-e=blockexplorer.com')
+print 'Getting balance of address %s from Blockexplorer.com' % address
+blockexplorer_com = spellbook_call('get_balance', address, '-e=blockexplorer.com')
 
 assert blockchain_info['balance']['final'] == blocktrail_com['balance']['final'] == blockexplorer_com['balance']['final']
 assert blockchain_info['balance']['received'] == blocktrail_com['balance']['received'] == blockexplorer_com['balance']['received']
@@ -95,13 +110,13 @@ assert blockchain_info['balance']['n_tx'] == blocktrail_com['balance']['n_tx'] =
 
 
 # --------------------------------------------------------------------------------------------------------
-print 'Getting utxos of address 1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8 from Blockchain.info'
-blockchain_info = spellbook_call('get_utxos', '1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8', '-e=blockchain.info')
+print 'Getting utxos of address %s from Blockchain.info' % address
+blockchain_info = spellbook_call('get_utxos', address, '-e=blockchain.info')
 
-print 'Getting utxos of address 1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8 from Blocktrail.com'
-blocktrail_com = spellbook_call('get_utxos', '1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8', '-e=blocktrail.com')
+print 'Getting utxos of address %s from Blocktrail.com' % address
+blocktrail_com = spellbook_call('get_utxos', address, '-e=blocktrail.com')
 
-print 'Getting utxos of address 1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8 from Blockexplorer.com'
-blockexplorer_com = spellbook_call('get_utxos', '1BAZ9hiAsMdSyw8CMeUoH4LeBnj7u6D7o8', '-e=blockexplorer.com')
+print 'Getting utxos of address %s from Blockexplorer.com' % address
+blockexplorer_com = spellbook_call('get_utxos', address, '-e=blockexplorer.com')
 
 assert len(blockchain_info['utxos']) == len(blocktrail_com['utxos']) == len(blockexplorer_com['utxos'])
