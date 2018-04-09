4498c0d942293f39b6bf828a6fbd9e6bc2725b32
Wouter Glorieux <wouter@valyrian.tech>
2018-04-09 22:24:28 +0200

    added hivemindhelpers added route for get_hivemind to spellbookserver

---
+++ helpers/hivemindhelpers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from helpers.jsonhelpers import load_from_json_file, save_to_json_file
+
+HIVEMINDS_DIR = 'json/public/hiveminds'
+HIVEMINDS_FILE = os.path.join(HIVEMINDS_DIR, 'hiveminds.json')
+
+# if hiveminds file doesn't exist create an empty one
+if not os.path.isfile(HIVEMINDS_FILE):
+    save_to_json_file(filename=HIVEMINDS_FILE, data={})
+
+
+def get_hivemind(hivemind_id):
+    hiveminds = load_from_json_file(filename=HIVEMINDS_FILE)
+
+    if hivemind_id in hiveminds:
+        return hiveminds[hivemind_id]
+
+
+def update_hivemind(hivemind_id, last_state_hash):
+    hiveminds = load_from_json_file(HIVEMINDS_FILE)
+
+    hiveminds[hivemind_id] = last_state_hash
+
+    save_to_json_file(filename=HIVEMINDS_FILE, data=hiveminds)
