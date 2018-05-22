c5d53d47648f7939b4f166be0af8357320ea27d8
Wouter Glorieux <wouter@valyrian.tech>
2018-05-22 21:59:17 +0200

    changed websocket to smartbit.com.au because that one also supports testnet

--- listeners/transaction_listener.py
+++ listeners/transaction_listener.py
@@ -10,30 +10,23 @@ WATCHLIST = {}
 
 def on_message(ws, message):
     transaction = simplejson.loads(message)
-    tx_hash = None
-    print '\n\nNew transaction:'
+    if transaction['type'] != 'new-transaction':
+        return
+
+    print '\n\nNew transaction:', transaction['payload']['txid']
     print 'From: '
-    for input_address in transaction['x']['inputs']:
-        if input_address['prev_out']['addr'] is not None:
-            print input_address['prev_out']['addr']
-            address = input_address['prev_out']['addr'][:2]
-            if address in WATCHLIST and 'SEND' in WATCHLIST[address]:
-                print 'Executing command: %s' % WATCHLIST[address]['SEND']
-        else:
-            tx_hash = transaction['x']['hash']
+    for tx_input in transaction['payload']['inputs']:
+        for input_address in tx_input['addresses']:
+            print input_address, tx_input['value_int']
+            if input_address in WATCHLIST and 'SEND' in WATCHLIST[input_address]:
+                print 'Executing command: %s' % WATCHLIST[input_address]['SEND']
 
     print 'To: '
-    for output_address in transaction['x']['out']:
-        if output_address['addr'] is not None:
-            print output_address['addr']
-            address = output_address['addr'][:2]
-            if address in WATCHLIST and 'RECEIVE' in WATCHLIST[address]:
-                print 'Executing command: %s' % WATCHLIST[address]['RECEIVE']
-        else:
-            tx_hash = transaction['x']['hash']
-
-    if tx_hash is not None:
-        print "===========> check tx", tx_hash
+    for tx_output in transaction['payload']['outputs']:
+        for output_address in tx_output['addresses']:
+            print output_address, tx_output['value_int']
+            if output_address in WATCHLIST and 'RECEIVE' in WATCHLIST[output_address]:
+                print 'Executing command: %s' % WATCHLIST[output_address]['RECEIVE']
 
 
 def on_error(ws, error):
@@ -41,20 +34,21 @@ def on_error(ws, error):
 
 
 def on_close(ws):
-    ws.send('{"op":"unconfirmed_unsub"}')
+    ws.send('{"type":"new-transaction", "unsubscribe": true}')
     print "### websocket closed ###"
 
 
 def on_open(ws):
     print "### websocket opened ###"
     print "Subscribing to new transactions"
-    ws.send('{"op":"unconfirmed_sub"}')
+    ws.send('{"type":"new-transaction"}')
 
 
 if __name__ == "__main__":
     # Create main parser
     parser = argparse.ArgumentParser(description='Transaction listener')
     parser.add_argument('-w', '--watchlist', help='the watchlist.json file containing the addresses and events to watch (default=watchlist.json', default='watchlist.json')
+    parser.add_argument('-t', '--testnet', help='Use testnet instead of mainnet', action='store_true')
 
     args = parser.parse_args()
 
@@ -68,11 +62,16 @@ if __name__ == "__main__":
     except IOError:
         raise Exception('File %s does not exists' % args.watchlist)
 
-    # websocket.enableTrace(True)
-    blockchain_info_websocket = websocket.WebSocketApp("wss://ws.blockchain.info/inv",
-                                                       on_open=on_open,
-                                                       on_message=on_message,
-                                                       on_error=on_error,
-                                                       on_close=on_close)
+    if args.testnet is True:
+        url = "wss://testnet-ws.smartbit.com.au/v1/blockchain"
+    else:
+        url = "wss://ws.smartbit.com.au/v1/blockchain"
 
-    blockchain_info_websocket.run_forever()
+    # websocket.enableTrace(True)
+    websocket = websocket.WebSocketApp(url=url,
+                                       on_open=on_open,
+                                       on_message=on_message,
+                                       on_error=on_error,
+                                       on_close=on_close)
+
+    websocket.run_forever()
