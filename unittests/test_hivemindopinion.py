2139d045f92293b476645273bc8c3ee78022a6f3
Wouter Glorieux <wouter@valyrian.tech>
2018-06-19 21:24:06 +0200

    added auto_complete option for integer an float type opinions

--- unittests/test_hivemindopinion.py
+++ unittests/test_hivemindopinion.py
@@ -25,17 +25,52 @@ hivemind_option = HivemindOption()
 hivemind_option.set_hivemind_question(STRING_QUESTION_HASH)
 hivemind_option.set('42')
 
-OPTION1_HASH = hivemind_option.save()
+STRING_OPTION1_HASH = hivemind_option.save()
 
 hivemind_option.set('fortytwo')
-OPTION2_HASH = hivemind_option.save()
+STRING_OPTION2_HASH = hivemind_option.save()
 
 hivemind_state = HivemindState()
 hivemind_state.set_hivemind_question(question_hash=STRING_QUESTION_HASH)
-hivemind_state.add_option(OPTION1_HASH)
-hivemind_state.add_option(OPTION2_HASH)
+hivemind_state.add_option(STRING_OPTION1_HASH)
+hivemind_state.add_option(STRING_OPTION2_HASH)
 
-STATE_HASH = hivemind_state.save()
+STRING_STATE_HASH = hivemind_state.save()
+
+hivemind_question = HivemindQuestion()
+hivemind_question.add_question(question='Choose a number')
+hivemind_question.set_description(description='Choose a number')
+hivemind_question.answer_type = 'Integer'
+hivemind_question.set_constraints({'min_value': 0, 'max_value': 10})
+INTEGER_QUESTION_HASH = hivemind_question.save()
+
+hivemind_option = HivemindOption()
+hivemind_option.set_hivemind_question(INTEGER_QUESTION_HASH)
+hivemind_option.set(8)
+
+INTEGER_OPTION1_HASH = hivemind_option.save()
+
+hivemind_option.set(5)
+INTEGER_OPTION2_HASH = hivemind_option.save()
+
+hivemind_option.set(6)
+INTEGER_OPTION3_HASH = hivemind_option.save()
+
+hivemind_option.set(7)
+INTEGER_OPTION4_HASH = hivemind_option.save()
+
+hivemind_option.set(4)
+INTEGER_OPTION5_HASH = hivemind_option.save()
+
+hivemind_state = HivemindState()
+hivemind_state.set_hivemind_question(question_hash=INTEGER_QUESTION_HASH)
+hivemind_state.add_option(INTEGER_OPTION1_HASH)
+hivemind_state.add_option(INTEGER_OPTION2_HASH)
+hivemind_state.add_option(INTEGER_OPTION3_HASH)
+hivemind_state.add_option(INTEGER_OPTION4_HASH)
+hivemind_state.add_option(INTEGER_OPTION5_HASH)
+
+INTEGER_STATE_HASH = hivemind_state.save()
 
 
 class TestHivemindOpinion(object):
@@ -46,22 +81,59 @@ class TestHivemindOpinion(object):
     def test_hivemind_state_is_set_before_setting_ranked_choice(self):
         opinion = HivemindOpinion()
         with pytest.raises(Exception):
-            opinion.set(opinionator='me', ranked_choice=[OPTION1_HASH, OPTION2_HASH])
+            opinion.set(opinionator='me', ranked_choice=[STRING_OPTION1_HASH, STRING_OPTION2_HASH])
 
-        opinion.set_hivemind_state(hivemind_state_hash=STATE_HASH)
-        opinion.set(opinionator='me', ranked_choice=[OPTION1_HASH, OPTION2_HASH])
+        opinion.set_hivemind_state(hivemind_state_hash=STRING_STATE_HASH)
+        opinion.set(opinionator='me', ranked_choice=[STRING_OPTION1_HASH, STRING_OPTION2_HASH])
 
     def test_setting_ranked_choice_only_contains_valid_option_hashes(self):
         opinion = HivemindOpinion()
-        opinion.set_hivemind_state(hivemind_state_hash=STATE_HASH)
+        opinion.set_hivemind_state(hivemind_state_hash=STRING_STATE_HASH)
         with pytest.raises(Exception):
-            opinion.set(opinionator='me', ranked_choice=[OPTION1_HASH, OPTION2_HASH, 'foo', 'bar'])
+            opinion.set(opinionator='me', ranked_choice=[STRING_OPTION1_HASH, STRING_OPTION2_HASH, 'foo', 'bar'])
 
         with pytest.raises(Exception):
-            opinion.set(opinionator='me', ranked_choice=OPTION1_HASH)  # must be a list of option hashes
+            opinion.set(opinionator='me', ranked_choice=STRING_OPTION1_HASH)  # must be a list of option hashes
 
     def test_setting_ranked_choice_does_not_contain_duplicate_option_hashes(self):
         opinion = HivemindOpinion()
-        opinion.set_hivemind_state(hivemind_state_hash=STATE_HASH)
+        opinion.set_hivemind_state(hivemind_state_hash=STRING_STATE_HASH)
         with pytest.raises(Exception):
-            opinion.set(opinionator='me', ranked_choice=[OPTION1_HASH, OPTION2_HASH, OPTION1_HASH])
+            opinion.set(opinionator='me', ranked_choice=[STRING_OPTION1_HASH, STRING_OPTION2_HASH, STRING_OPTION1_HASH])
+
+    def test_auto_complete(self):
+        opinion = HivemindOpinion()
+        opinion.set_hivemind_state(hivemind_state_hash=INTEGER_STATE_HASH)
+        opinion.set(opinionator='me', ranked_choice=[INTEGER_OPTION3_HASH])
+        assert opinion.get() == [INTEGER_OPTION3_HASH]
+
+        print '\nAuto_completing using opinion as MAX value'
+        opinion.auto_complete = 'MAX'
+        for option_hash in opinion.get():
+            print HivemindOption(option_hash=option_hash).get(), option_hash
+        assert opinion.get() == [INTEGER_OPTION5_HASH, INTEGER_OPTION2_HASH, INTEGER_OPTION3_HASH]
+
+        print '\nAuto_completing using opinion as MIN value'
+        opinion.auto_complete = 'MIN'
+        for option_hash in opinion.get():
+            print HivemindOption(option_hash=option_hash).get(), option_hash
+        assert opinion.get() == [INTEGER_OPTION3_HASH, INTEGER_OPTION4_HASH, INTEGER_OPTION1_HASH]
+
+        print '\nAuto_completing using opinion as CLOSEST value if 2 other options are equally close, the order of adding them to the hivemind is used'
+        opinion.auto_complete = 'CLOSEST'
+        for option_hash in opinion.get():
+            print HivemindOption(option_hash=option_hash).get(), option_hash
+        assert opinion.get() == [INTEGER_OPTION3_HASH, INTEGER_OPTION2_HASH, INTEGER_OPTION4_HASH, INTEGER_OPTION1_HASH, INTEGER_OPTION5_HASH]
+
+        print '\nAuto_completing using opinion as CLOSEST value with preference for higher values if equally close'
+        opinion.auto_complete = 'CLOSEST_HIGH'
+        for option_hash in opinion.get():
+            print HivemindOption(option_hash=option_hash).get(), option_hash
+        assert opinion.get() == [INTEGER_OPTION3_HASH, INTEGER_OPTION4_HASH, INTEGER_OPTION2_HASH, INTEGER_OPTION1_HASH, INTEGER_OPTION5_HASH]
+
+        print '\nAuto_completing using opinion as CLOSEST value with preference for lower values if equally close'
+        opinion.auto_complete = 'CLOSEST_LOW'
+        for option_hash in opinion.get():
+            print HivemindOption(option_hash=option_hash).get(), option_hash
+        assert opinion.get() == [INTEGER_OPTION3_HASH, INTEGER_OPTION2_HASH, INTEGER_OPTION4_HASH, INTEGER_OPTION5_HASH, INTEGER_OPTION1_HASH]
+
