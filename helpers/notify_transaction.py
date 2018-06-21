13d65237a83dc4c759bdd2c3ca0ee01536ebd3c9
Wouter Glorieux <wouter@valyrian.tech>
2018-06-21 19:50:59 +0200

    a helper program to easily make a POST request with json data via curl

---
+++ helpers/notify_transaction.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+from subprocess import Popen, PIPE
+
+if __name__ == "__main__":
+    # Create main parser
+    parser = argparse.ArgumentParser(description='Notify transaction')
+    parser.add_argument('url', help='The url to send the notification to')
+    parser.add_argument('pr', help='The id of the payment request')
+    parser.add_argument('txid', help='The transaction id')
+
+    args = parser.parse_args()
+
+    command = r'curl %s -H "Content-Type: application/json" -d "{\"payment_request_id\":\"%s\",\"txid\":\"%s\"}"' % (args.url, args.pr, args.txid)
+
+    command_process = Popen(command, stdout=PIPE, stderr=PIPE, shell=True)
+    output, error = command_process.communicate()
+
