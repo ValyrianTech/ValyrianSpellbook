bf076e1207eac1543b75e7d0e643a502c8f5c83c
Wouter Glorieux <wouter@valyrian.tech>
2018-08-14 17:27:37 +0200

    refactored the compare_explorers script

--- integrationtests/compare_explorers.py
+++ integrationtests/compare_explorers.py
@@ -1,54 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 import argparse
 import time
-import copy
-from integration_test_helpers import spellbook_call
+import deepdiff
+
+from helpers.setupscripthelpers import spellbook_call
 from pprint import pprint
 
 
-KEYNOTFOUNDIN1 = '<KEYNOTFOUNDIN1>'       # KeyNotFound for dictDiff
-KEYNOTFOUNDIN2 = '<KEYNOTFOUNDIN2>'       # KeyNotFound for dictDiff
-
-
-def dict_diff(first, second):
-    """ Return a dict of keys that differ with another config object.  If a value is
-        not found in one fo the configs, it will be represented by KEYNOTFOUND.
-        @param first:   Fist dictionary to diff.
-        @param second:  Second dictionary to diff.
-        @return diff:   Dict of Key => (first.val, second.val)
-    """
-    diff = {}
-    sd1 = set(first)
-    sd2 = set(second)
-    # Keys missing in the second dict
-    for key in sd1.difference(sd2):
-        diff[key] = KEYNOTFOUNDIN2
-    # Keys missing in the first dict
-    for key in sd2.difference(sd1):
-        diff[key] = KEYNOTFOUNDIN1
-    # Check for differences
-    for key in sd1.intersection(sd2):
-        if first[key] != second[key]:
-            diff[key] = (first[key], second[key])
-    return diff
+EXPLORERS = spellbook_call('get_explorers')
 
 
 class Comparison(object):
     def __init__(self, *args):
-        self.explorers = spellbook_call('get_explorers')
         self.spellbook_args = list(args)
 
+        print '\n\n#####################################################################################################'
+        print '# Comparing: %s' % self.spellbook_args
+        print '#####################################################################################################'
+
         self.responses = {}
-        for explorer in self.explorers:
+        for explorer in EXPLORERS:
             self.responses[explorer] = self.get_response_from_explorer(explorer)
 
         self.compare_response_times()
-        ok = self.compare_response_data()
-        if not ok:
-            sys.exit(1)
+        self.compare_response_data()
+        print '---------------------------------------------------------------------------'
 
     def get_response_from_explorer(self, explorer):
         start_time = time.time()
@@ -65,35 +43,17 @@ class Comparison(object):
 
         print '\nComparing response data:'
         response_data = {explorer: self.responses[explorer][0] for explorer in self.responses}
-        keys = [key for key in response_data[self.explorers[0]].keys() if key != 'explorer']
 
-        for i in range(len(self.explorers)):
+        for i in range(len(EXPLORERS)):
+            print '\nComparing %s and %s: .... ' % (EXPLORERS[i], EXPLORERS[i-1]),
+            differences = deepdiff.DeepDiff(response_data[EXPLORERS[i]], response_data[EXPLORERS[i-1]])
 
-            for key in keys:
-
-                print ''
-                print 'comparing key %s from %s and %s: ' % (key, self.explorers[i], self.explorers[i - 1]),
-                first = copy.deepcopy(response_data[self.explorers[i]][key])
-                second = copy.deepcopy(response_data[self.explorers[i-1]][key])
-
-                if cmp(first, second) != 0:
-                    print 'Not equal!'
-                    if isinstance(first, dict) and isinstance(second, dict):
-                        print dict_diff(first, second)
-                    elif isinstance(first, list) and isinstance(second, list):
-                        for j in range(len(first)):
-                            if isinstance(first[j], dict) and isinstance(second[j], dict):
-                                difference = dict_diff(first[j], second[j])
-                                if difference:
-                                    print 'transaction is different!!'
-                                    print first[j]['txid']
-                                    print second[j]['txid']
-                                    pprint(difference)
-
-                    all_ok = False
-
-                else:
-                    print 'OK'
+            if len(differences['values_changed'].keys()) > 1 or 'type_changes' in differences:
+                print 'NOT OK'
+                pprint(differences)
+                all_ok = False
+            else:
+                print 'OK'
 
         return all_ok
 
@@ -104,6 +64,6 @@ if __name__ == "__main__":
     parser.add_argument('args', help='The arguments for the spellbook.py command', nargs='+')
 
     # Parse the command line arguments
-    args = parser.parse_args()
+    compare_args = parser.parse_args()
 
-    Comparison(*args.args)
+    Comparison(*compare_args.args)
