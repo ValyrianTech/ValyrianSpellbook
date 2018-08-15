47ef8700ade599eccd3bb4af7b47446fb79fe4ad
Wouter Glorieux <wouter@valyrian.tech>
2018-08-15 14:08:52 +0200

    use absolute path instead of relative path renamed LOG to LISTENER_LOG to avoid confusion with spellbook log

--- listeners/transaction_listener.py
+++ listeners/transaction_listener.py
@@ -11,22 +11,24 @@ import logging
 from logging.handlers import RotatingFileHandler
 from helpers.runcommandprocess import RunCommandProcess
 
-LOG = logging.getLogger('transaction_listener_log')
+PROGRAM_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+LISTENER_LOG = logging.getLogger('transaction_listener_log')
 
 stream_handler = logging.StreamHandler(sys.stdout)
 stream_handler.setFormatter(logging.Formatter('%(asctime)s | %(levelname)s | %(message)s'))
-LOG.addHandler(stream_handler)
+LISTENER_LOG.addHandler(stream_handler)
 
 # make the directory for logs if it doesn't exist
-logs_dir = os.path.join('logs')
+logs_dir = os.path.join(PROGRAM_DIR, 'logs')
 if not os.path.isdir(logs_dir):
     os.makedirs(logs_dir)
 
-file_handler = RotatingFileHandler(os.path.join('logs', 'transaction_listener_log.txt'), maxBytes=10000000, backupCount=5)
+file_handler = RotatingFileHandler(os.path.join(PROGRAM_DIR, 'logs', 'transaction_listener_log.txt'), maxBytes=10000000, backupCount=5)
 file_handler.setFormatter(logging.Formatter('%(asctime)s | %(levelname)s | %(message)s'))
-LOG.addHandler(file_handler)
+LISTENER_LOG.addHandler(file_handler)
 
-LOG.setLevel(logging.WARNING)
+LISTENER_LOG.setLevel(logging.WARNING)
 
 WATCHLIST = {}
 EXIT_ON_EVENT = False
@@ -39,55 +41,55 @@ def on_message(ws, message):
     if transaction['type'] != 'new-transaction':
         return
 
-    LOG.info('New transaction: %s' % transaction['payload']['txid'])
-    LOG.info('\tFrom: ')
+    LISTENER_LOG.info('New transaction: %s' % transaction['payload']['txid'])
+    LISTENER_LOG.info('\tFrom: ')
     for tx_input in transaction['payload']['inputs']:
         for input_address in tx_input['addresses']:
-            LOG.info('\t\t%s -> %s' % (input_address, tx_input['value_int']))
+            LISTENER_LOG.info('\t\t%s -> %s' % (input_address, tx_input['value_int']))
             if input_address in WATCHLIST and 'SEND' in WATCHLIST[input_address]:
 
                 event_found = True
                 command = WATCHLIST[input_address]['SEND']
                 command = command.replace('#txid#', transaction['payload']['txid'])
-                LOG.info('Executing command: %s' % command)
+                LISTENER_LOG.info('Executing command: %s' % command)
                 run_command_process = RunCommandProcess(command=command)
                 run_command_process.start()
 
-    LOG.info('\tTo: ')
+    LISTENER_LOG.info('\tTo: ')
     for tx_output in transaction['payload']['outputs']:
         for output_address in tx_output['addresses']:
-            LOG.info('\t\t%s -> %s' % (output_address, tx_output['value_int']))
+            LISTENER_LOG.info('\t\t%s -> %s' % (output_address, tx_output['value_int']))
             if output_address in WATCHLIST and 'RECEIVE' in WATCHLIST[output_address]:
 
                 event_found = True
                 command = WATCHLIST[output_address]['RECEIVE']
                 command = command.replace('#txid#', transaction['payload']['txid'])
-                LOG.info('Executing command: %s' % command)
+                LISTENER_LOG.info('Executing command: %s' % command)
                 run_command_process = RunCommandProcess(command=command)
                 run_command_process.start()
 
     if EXIT_ON_EVENT is True and event_found is True:
-        LOG.info('Event found, exiting now')
+        LISTENER_LOG.info('Event found, exiting now')
         ws.send('{"type":"new-transaction", "unsubscribe": true}')
         sys.exit()
 
     if EXIT_ON_TIMEOUT is not None and int(time.time()) >= EXIT_ON_TIMEOUT:
-        LOG.info('Timeout occurred, exiting now')
+        LISTENER_LOG.info('Timeout occurred, exiting now')
         ws.send('{"type":"new-transaction", "unsubscribe": true}')
         sys.exit()
 
 
 def on_error(ws, error):
-    LOG.info('ERROR: %s' % error)  # use info level here instead of error level because for some reason an error is raised when the program exits
+    LISTENER_LOG.info('ERROR: %s' % error)  # use info level here instead of error level because for some reason an error is raised when the program exits
 
 
 def on_close(ws):
-    LOG.info("### websocket closed ###")
+    LISTENER_LOG.info("### websocket closed ###")
 
 
 def on_open(ws):
-    LOG.info("### websocket opened ###")
-    LOG.info("Subscribing to new transactions")
+    LISTENER_LOG.info("### websocket opened ###")
+    LISTENER_LOG.info("Subscribing to new transactions")
     ws.send('{"type":"new-transaction"}')
 
 
@@ -109,7 +111,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.verbose is True:
-        LOG.setLevel(logging.INFO)
+        LISTENER_LOG.setLevel(logging.INFO)
 
     EXIT_ON_EVENT = args.exit
 
@@ -139,9 +141,9 @@ if __name__ == "__main__":
     else:
         url = "wss://ws.smartbit.com.au/v1/blockchain"
 
-    LOG.info('Starting transaction listener')
-    LOG.info('Watchlist:')
-    LOG.info(WATCHLIST)
+    LISTENER_LOG.info('Starting transaction listener')
+    LISTENER_LOG.info('Watchlist:')
+    LISTENER_LOG.info(WATCHLIST)
 
     # websocket.enableTrace(True)
     websocket = websocket.WebSocketApp(url=url,
