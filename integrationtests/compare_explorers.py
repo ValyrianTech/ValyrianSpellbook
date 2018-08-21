6cac11a925b57bec435f7ae6fc930c46fe852343
Wouter Glorieux <wouter@valyrian.tech>
2018-08-21 15:32:02 +0200

    added some methods to compare differences easier

--- integrationtests/compare_explorers.py
+++ integrationtests/compare_explorers.py
@@ -25,7 +25,58 @@ class Comparison(object):
             self.responses[explorer] = self.get_response_from_explorer(explorer)
 
         self.compare_response_times()
-        self.compare_response_data()
+
+        if args[0] == 'get_transaction':
+            transactions = {explorer: self.responses[explorer][0]['transaction'] for explorer in self.responses}
+            self.compare_transaction(transactions=transactions)
+        elif args[0] == 'get_transactions':
+            all_ok = True
+            # check each transaction
+            for i in range(len(self.responses[self.responses.keys()[0]][0]['transactions'])):
+                print '\n------------------------------------------------------------------'
+                print 'Transaction %s' % i
+                transactions = {explorer: self.responses[explorer][0]['transactions'][i] for explorer in self.responses}
+                ok = self.compare_transaction(transactions=transactions)
+                if ok is False:
+                    all_ok = False
+
+            # check the number of transactions
+            n_txs = [len(self.responses[explorer][0]['transactions']) for explorer in self.responses]
+            if not all(n_txs[0] == item for item in n_txs):
+                print 'different number of transactions by some explorers'
+                for explorer in self.responses:
+                    print '%s: %' % (explorer, len(self.responses[explorer])[0]['transactions'])
+
+            if all_ok is True:
+                print 'All transactions are the same on all explorers'
+        elif args[0] == 'get_utxos':
+            all_ok = True
+            # check each utxo
+            for i in range(len(self.responses[self.responses.keys()[0]][0]['utxos'])):
+                print '\n------------------------------------------------------------------'
+                print 'UTXO %s' % i
+                dicts = {explorer: self.responses[explorer][0]['utxos'][i] for explorer in self.responses}
+                ok = self.compare_dict_items(dicts=dicts, item_names=['confirmations', 'output_hash', 'output_n', 'value', 'script'])
+                if ok is False:
+                    all_ok = False
+
+            # check the number of utxos
+            n_txs = [len(self.responses[explorer][0]['utxos']) for explorer in self.responses]
+            if not all(n_txs[0] == item for item in n_txs):
+                print 'different number of utxos by some explorers'
+                for explorer in self.responses:
+                    print '%s: %' % (explorer, len(self.responses[explorer])[0]['utxos'])
+
+        elif args[0] in ['get_block', 'get_latest_block']:
+            print '\n------------------------------------------------------------------'
+            print 'Block'
+            dicts = {explorer: self.responses[explorer][0]['block'] for explorer in self.responses}
+            ok = self.compare_dict_items(dicts=dicts, item_names=['hash', 'height', 'merkleroot', 'time', 'size'])
+            if ok is False:
+                all_ok = False
+
+        else:
+            self.compare_response_data()
         print '---------------------------------------------------------------------------'
 
     def get_response_from_explorer(self, explorer):
@@ -54,6 +105,55 @@ class Comparison(object):
                 all_ok = False
             else:
                 print 'OK'
+
+        return all_ok
+
+    def compare_transaction(self, transactions):
+        """
+        Compare a transaction as received by each explorer
+
+        :param transactions: a dict containing the transaction as received by each explorer
+        """
+        all_ok = True
+        first_explorer = transactions.keys()[0]
+
+        print '\nComparing transaction'
+        dicts = transactions
+        ok = self.compare_dict_items(dicts=dicts, item_names=['txid', 'block_height', 'confirmations', 'prime_input_address', 'lock_time'])  # wtxid only given by btc.com
+        if ok is False:
+            all_ok = False
+
+        n_inputs = len(transactions[first_explorer]['inputs'])
+        for i in range(n_inputs):
+            print '\nComparing input %s' % i
+            dicts = {explorer: transactions[explorer]['inputs'][i] for explorer in transactions}
+            ok = self.compare_dict_items(dicts=dicts, item_names=['address', 'value', 'n', 'script'])  # txid is not present in responses from blockchain.info, sequence not given by blocktrail
+            if ok is False:
+                all_ok = False
+
+        n_outputs = len(transactions[first_explorer]['outputs'])
+        for i in range(n_outputs):
+            print '\nComparing output %s' % i
+            dicts = {explorer: transactions[explorer]['outputs'][i] for explorer in transactions}
+            ok = self.compare_dict_items(dicts=dicts, item_names=['address', 'value', 'n', 'script', 'op_return', 'spent'])
+            if ok is False:
+                all_ok = False
+
+        if all_ok is True:
+            print 'transaction is the same on all explorers'
+
+        return all_ok
+
+    def compare_dict_items(self, dicts, item_names):
+        all_ok = True
+        for item_name in item_names:
+            print '\nChecking %s' % item_name
+            # check if all items are the same
+            items = [dicts[explorer][item_name] for explorer in dicts]
+            if not all(items[0] == item for item in items):
+                all_ok = False
+                for explorer in self.responses:
+                    print '%s -> %s: %s' % (explorer, item_name, dicts[explorer][item_name])
 
         return all_ok
 
