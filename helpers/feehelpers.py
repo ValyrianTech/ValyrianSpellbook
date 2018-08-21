cb98d51c7ddc042f8e2d841691a2fe8644c72f4d
Wouter Glorieux <wouter@valyrian.tech>
2018-08-21 23:48:12 +0200

    added get_recommended_fee() in case blocktrail is not configured

--- helpers/feehelpers.py
+++ helpers/feehelpers.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import requests
 
 from data.data import get_explorer_api
+from helpers.loghelpers import LOG
 
 
 def get_optimal_fee():
     blocktrail = get_explorer_api('blocktrail.com')
-    data = blocktrail.get_recommended_fee()
+    data = blocktrail.get_recommended_fee() if blocktrail is not None else get_recommended_fee()
     return int(data['optimal']/1024)
 
 
 def get_low_priority_fee():
     blocktrail = get_explorer_api('blocktrail.com')
-    data = blocktrail.get_recommended_fee()
+    data = blocktrail.get_recommended_fee() if blocktrail is not None else get_recommended_fee()
     return int(data['low_priority']/1024)
 
 
 def get_high_priority_fee():
     blocktrail = get_explorer_api('blocktrail.com')
-    data = blocktrail.get_recommended_fee()
+    data = blocktrail.get_recommended_fee() if blocktrail is not None else get_recommended_fee()
     return int(data['high_priority']/1024)
+
+
+def get_recommended_fee():
+    url = 'https://bitcoinfees.earn.com/api/v1/fees/recommended'
+
+    try:
+        LOG.info('GET %s' % url)
+        r = requests.get(url=url)
+        data = r.json()
+    except Exception as ex:
+        raise Exception('Unable get recommended fee from bitcoinfees.earn.com: %s' % ex)
+
+    return {'high_priority': data['fastestFee']*1024,
+            'low_priority': data['hourFee']*1024,
+            'optimal': data['halfHourFee']*1024}
