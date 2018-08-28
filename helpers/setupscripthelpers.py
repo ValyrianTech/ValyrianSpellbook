233a31b0a49a46e340685d830bf87f1b5bd278bd
Wouter Glorieux <wouter@valyrian.tech>
2018-08-28 20:23:05 +0200

    if the arguments for a process on linux contain a space , the argument must be surrounced by quotes

--- helpers/setupscripthelpers.py
+++ helpers/setupscripthelpers.py
@@ -78,9 +78,15 @@ def clean_up_actions(action_ids):
 def format_args(args):
     """
     Format the args to pass to the subprocess
-    Linux requires a string with spaces, whereas Windows requires a list
+    Linux requires a string with spaces (if an argument contains spaces it must be surrounded with quotes), whereas Windows requires a list
 
     :param args: A list of arguments
     :return: The arguments as required by the operating system
     """
-    return ' '.join(args) if platform.system() == 'Linux' else args
+    if platform.system() == 'Linux':
+        formatted_string = ''
+        for arg in args:
+            formatted_string += '%s ' % arg if ' ' not in arg else '"%s" ' % arg
+        return formatted_string
+    else:
+        return args
