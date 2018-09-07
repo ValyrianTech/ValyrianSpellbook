3fb678288bd1a4d60af0a3a47f99f53dda8648e8
Wouter Glorieux <wouter@valyrian.tech>
2018-09-07 16:18:48 +0200

    get recommended fee from blockcypher because they provide both mainnet and testnet fee estimates

--- helpers/feehelpers.py
+++ helpers/feehelpers.py
@@ -4,23 +4,21 @@ import requests
 
 from data.data import get_explorer_api
 from helpers.loghelpers import LOG
+from helpers.configurationhelpers import get_use_testnet
 
 
 def get_optimal_fee():
-    blocktrail = get_explorer_api('blocktrail.com')
-    data = blocktrail.get_recommended_fee() if blocktrail is not None else get_recommended_fee()
+    data = get_recommended_fee_blockcypher()
     return int(data['optimal']/1024)
 
 
 def get_low_priority_fee():
-    blocktrail = get_explorer_api('blocktrail.com')
-    data = blocktrail.get_recommended_fee() if blocktrail is not None else get_recommended_fee()
+    data = get_recommended_fee_blockcypher()
     return int(data['low_priority']/1024)
 
 
 def get_high_priority_fee():
-    blocktrail = get_explorer_api('blocktrail.com')
-    data = blocktrail.get_recommended_fee() if blocktrail is not None else get_recommended_fee()
+    data = get_recommended_fee_blockcypher()
     return int(data['high_priority']/1024)
 
 
@@ -37,3 +35,18 @@ def get_recommended_fee():
     return {'high_priority': data['fastestFee']*1024,
             'low_priority': data['hourFee']*1024,
             'optimal': data['halfHourFee']*1024}
+
+
+def get_recommended_fee_blockcypher():
+    url = 'https://api.blockcypher.com/v1/btc/test3' if get_use_testnet() is True else 'https://api.blockcypher.com/v1/btc/main'
+
+    try:
+        LOG.info('GET %s' % url)
+        r = requests.get(url=url)
+        data = r.json()
+    except Exception as ex:
+        raise Exception('Unable get recommended fee from blockcypher.com: %s' % ex)
+
+    return {'high_priority': data['high_fee_per_kb'],
+            'low_priority': data['low_fee_per_kb'],
+            'optimal': data['medium_fee_per_kb']}
