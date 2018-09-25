9393b089742dcbd299bca3b605e12aa4aa8cb790
Wouter Glorieux <wouter@valyrian.tech>
2018-09-25 17:16:11 +0200

    changed default directories

--- install-spellbook.sh
+++ install-spellbook.sh
@@ -4,12 +4,8 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install git python2.7 python-dev python-pip -y
 
-export PATH=$PATH:~/.local/bin
-
-mkdir spellbook
-cd spellbook
-mkdir wallet
-mkdir app_data
+mkdir spellbook_wallet
+mkdir spellbook_data
 
 git clone https://github.com/ValyrianTech/BitcoinSpellbook-v0.3.git spellbook
 cd spellbook
