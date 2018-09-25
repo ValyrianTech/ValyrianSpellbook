14dad8df493c7eccc9001c146273cb7916556c28
Wouter Glorieux <wouter@valyrian.tech>
2018-09-25 17:00:10 +0200

    added export fixed wrong command for installing requirements

--- install-spellbook.sh
+++ install-spellbook.sh
@@ -3,6 +3,9 @@ cd ~
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install git python2.7 python-dev python-pip -y
+
+export PATH=$PATH:~/.local/bin
+
 mkdir spellbook
 cd spellbook
 mkdir wallet
@@ -10,5 +13,5 @@ mkdir app_data
 
 git clone https://github.com/ValyrianTech/BitcoinSpellbook-v0.3.git spellbook
 cd spellbook
-pip install pipreqs
-pipreqs install -r requirements.txt
+
+pip install -r requirements.txt
