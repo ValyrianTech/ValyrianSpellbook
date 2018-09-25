b0e1d74055f0a1145ef27e4318c8e5f8852e4d9f
Wouter Glorieux <wouter@valyrian.tech>
2018-09-25 16:08:17 +0200

    added install shell script

---
+++ install-spellbook.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install git python2.7 python-dev
+
+mkdir spellbook
+cd spellbook
+
+mkdir wallet
+mkdir app_data
+
+
+git init
+git clone https://github.com/ValyrianTech/BitcoinSpellbook-v0.3.git spellbook
+
+cd spellbook
+
+python install pip
+pip install pipreqs
+pipreqs install -r requirements.txt
