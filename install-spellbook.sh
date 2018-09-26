087391ce8150d97755f7e724a3b4db9c219d0dea
Wouter Glorieux <wouter@valyrian.tech>
2018-09-26 14:26:14 +0200

    change default directories for configuration file

--- install-spellbook.sh
+++ install-spellbook.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # run this script on a fresh linux machine to install the Bitcoin Spellbook and all requirements
-# Before running this script, create a new user with sudo rights and run this script as that user from the home directory.
+# Before running this script, create a new user named spellbook with sudo rights and run this script as that user from the home directory.
 #
-# sudo adduser username
-# usermod -aG sudo username
-# su username
+# sudo adduser spellbook
+# usermod -aG sudo spellbook
+# su spellbook
 # cd ~
 # wget https://raw.githubusercontent.com/ValyrianTech/BitcoinSpellbook-v0.3/master/install-spellbook.sh
 # sh install-spellbook.h
