3c209b8f89255cd946c38175aed27b3fe158ab1d
Wouter Glorieux <wouter@valyrian.tech>
2019-08-05 21:15:07 +0200

    added methods to retrieve blocks

--- data/blockexplorers/blockstream.py
+++ data/blockexplorers/blockstream.py
@@ -12,16 +12,51 @@ class BlockstreamAPI(ExplorerAPI):
     def __init__(self, url='', key='', testnet=False):
         super(BlockstreamAPI, self).__init__(url=url, testnet=testnet)
         # Set the url of the api depending on testnet or mainnet
-        self.url = 'https://blockstream.info/testnet' if self.testnet is True else 'https://blockstream.info'
+        self.url = 'https://blockstream.info/testnet/api' if self.testnet is True else 'https://blockstream.info/api'
 
     def get_latest_block(self):
-        pass
+        url = self.url + '/blocks/tip/hash'
+        LOG.info('GET %s' % url)
+        try:
+            r = requests.get(url)
+            block_hash = r.text
+        except Exception as ex:
+            LOG.error('Unable to get latest block_hash from Blockstream.info: %s' % ex)
+            return {'error': 'Unable to get latest block_hash from Blockstream.info'}
+
+        return self.get_block_by_hash(block_hash=block_hash)
 
     def get_block_by_hash(self, block_hash):
-        pass
+        url = self.url + '/block/{hash}'.format(hash=block_hash)
+        LOG.info('GET %s' % url)
+        try:
+            r = requests.get(url)
+            data = r.json()
+        except Exception as ex:
+            LOG.error('Unable to get block %s from Blockstream.info: %s' % (block_hash, ex))
+            return {'error': 'Unable to get block %s from Blockstream.info' % block_hash}
+
+        if all(key in data for key in ('height', 'id', 'timestamp', 'merkle_root', 'size')):
+            block = {'height': data['height'],
+                     'hash': data['id'],
+                     'time': data['timestamp'],
+                     'merkleroot': data['merkle_root'],
+                     'size': data['size']}  # Todo weight?
+            return {'block': block}
+        else:
+            return {'error': 'Received invalid data: %s' % data}
 
     def get_block_by_height(self, height):
-        pass
+        url = self.url + '/block-height/{height}'.format(height=height)
+        LOG.info('GET %s' % url)
+        try:
+            r = requests.get(url)
+            block_hash = r.text
+        except Exception as ex:
+            LOG.error('Unable to get block %s from Blockstream.info: %s' % (height, ex))
+            return {'error': 'Unable to get block %s from Blockstream.info' % height}
+
+        return self.get_block_by_hash(block_hash=block_hash)
 
     def get_transactions(self, address):
         pass
