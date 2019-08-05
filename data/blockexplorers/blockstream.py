11fdb8ac78dff2ba891ae5912f642be1a5f525de
Wouter Glorieux <wouter@valyrian.tech>
2019-08-05 21:46:40 +0200

    added method get_transaction

--- data/blockexplorers/blockstream.py
+++ data/blockexplorers/blockstream.py
@@ -65,7 +65,45 @@ class BlockstreamAPI(ExplorerAPI):
         pass
 
     def get_transaction(self, txid):
-        pass
+        url = self.url + '/tx/{txid}'.format(txid=txid)
+        LOG.info('GET %s' % url)
+        try:
+            r = requests.get(url)
+            data = r.json()
+        except Exception as ex:
+            LOG.error('Unable to get transaction %s from Blockstream.info: %s' % (txid, ex))
+            return {'error': 'Unable to get transaction %s from Blockstream.info' % txid}
+
+        tx = TX()
+        tx.txid = txid
+        tx.lock_time = data['locktime']
+        tx.block_height = data['status']['block_height'] if 'block_height' in data['status'] else None
+        tx.confirmations = self.get_latest_block_height() - tx.block_height + 1 if tx.block_height is not None else 0
+
+        for item in data['vin']:
+            tx_input = TxInput()
+            tx_input.address = item['prevout']['scriptpubkey_address'] if 'prevout' in item else None
+            tx_input.value = item['prevout']['value'] if 'prevout' in item else 0
+            tx_input.n = item['vout']
+            tx_input.txid = item['txid']
+            tx_input.script = item['scriptsig']
+            tx_input.sequence = item['sequence']
+
+            tx.inputs.append(tx_input)
+
+        for i, item in enumerate(data['vout']):
+            tx_output = TxOutput()
+            tx_output.address = item['scriptpubkey_address'] if 'scriptpubkey_address' in item else None
+            tx_output.value = item['value']
+            tx_output.n = i
+            tx_output.spent = None  # Blockstream does not provide information if a tx output has been spent
+            tx_output.script = item['scriptpubkey']
+            if item['scriptpubkey'][:2] == '6a':
+                tx_output.op_return = tx.decode_op_return(item['scriptpubkey'])
+
+            tx.outputs.append(tx_output)
+
+        return {'transaction': tx.json_encodable()}
 
     def get_prime_input_address(self, txid):
         pass
