c9d3981174eac8dbbd230a484c0996283d95fa31
Wouter Glorieux <wouter@valyrian.tech>
2019-08-16 14:30:13 +0200

    python2/3 compatibility

--- integrationtests/integrationtest_action_RevealSecret.py
+++ integrationtests/integrationtest_action_RevealSecret.py
@@ -3,8 +3,8 @@
 from helpers.setupscripthelpers import spellbook_call, clean_up_actions
 
 
-print 'Starting Spellbook integration test: RevealSecret actions'
-print '----------------------------------------------\n'
+print('Starting Spellbook integration test: RevealSecret actions')
+print('----------------------------------------------\n')
 
 # Clean up actions if necessary
 clean_up_actions(action_ids=['integrationtest_action_RevealSecret'])
@@ -18,17 +18,17 @@ reveal_link = 'http://www.asecretlink.com'
 
 # --------------------------------------------------------------------------------------------------------
 
-print 'Creating test action: RevealSecret'
+print('Creating test action: RevealSecret')
 response = spellbook_call('save_action', '-t=RevealSecret', action_name, '-rt=%s' % reveal_text, '-rl=%s' % reveal_link)
 assert response is None
 
 # --------------------------------------------------------------------------------------------------------
-print 'Getting the list of configured action_ids'
+print('Getting the list of configured action_ids')
 response = spellbook_call('get_actions')
 assert action_name in response
 
 # --------------------------------------------------------------------------------------------------------
-print 'Getting the action config of the action we just created'
+print('Getting the action config of the action we just created')
 response = spellbook_call('get_action_config', action_name)
 assert response['id'] == action_name
 assert response['action_type'] == 'RevealSecret'
@@ -36,17 +36,17 @@ assert response['reveal_text'] == reveal_text
 assert response['reveal_link'] == reveal_link
 
 # --------------------------------------------------------------------------------------------------------
-print 'Checking if reveal is none because action has not been activated yet'
+print('Checking if reveal is none because action has not been activated yet')
 response = spellbook_call('get_reveal', action_name)
 assert response is None
 
 # --------------------------------------------------------------------------------------------------------
-print 'Activating the action we just created'
+print('Activating the action we just created')
 response = spellbook_call('run_action', action_name)
 assert response is True
 
 # --------------------------------------------------------------------------------------------------------
-print 'Checking if reveal is given because action has been activated'
+print('Checking if reveal is given because action has been activated')
 response = spellbook_call('get_reveal', action_name)
 assert response['reveal_text'] == reveal_text
 assert response['reveal_link'] == reveal_link
