8e12d541f67fde37e814b308f2ab7d90342d5471
Wouter Glorieux <wouter@valyrian.tech>
2019-08-16 14:37:11 +0200

    python2/3 compatibility

--- integrationtests/integrationtest_action_Webhook.py
+++ integrationtests/integrationtest_action_Webhook.py
@@ -3,8 +3,8 @@
 from helpers.setupscripthelpers import spellbook_call, clean_up_actions
 
 
-print 'Starting Spellbook integration test: Webhook actions'
-print '----------------------------------------------\n'
+print('Starting Spellbook integration test: Webhook actions')
+print('----------------------------------------------\n')
 
 # Clean up actions if necessary
 clean_up_actions(action_ids=['integrationtest_action_Webhook'])
@@ -17,24 +17,24 @@ webhook = 'http://www.google.com'
 
 # --------------------------------------------------------------------------------------------------------
 
-print 'Creating test action: Webhook'
+print('Creating test action: Webhook')
 response = spellbook_call('save_action', '-t=Webhook', action_name, '-w=%s' % webhook)
 assert response is None
 
 # --------------------------------------------------------------------------------------------------------
-print 'Getting the list of configured action_ids'
+print('Getting the list of configured action_ids')
 response = spellbook_call('get_actions')
 assert action_name in response
 
 # --------------------------------------------------------------------------------------------------------
-print 'Getting the action config of the action we just created'
+print('Getting the action config of the action we just created')
 response = spellbook_call('get_action_config', action_name)
 assert response['id'] == action_name
 assert response['action_type'] == 'Webhook'
 assert response['webhook'] == webhook
 
 # --------------------------------------------------------------------------------------------------------
-print 'Running the action we just created'
+print('Running the action we just created')
 response = spellbook_call('run_action', action_name)
 assert response is True
 
@@ -48,6 +48,6 @@ assert response['id'] == action_name
 assert response['action_type'] == 'Webhook'
 assert response['webhook'] == webhook
 
-print 'Running the action with a bad url'
+print('Running the action with a bad url')
 response = spellbook_call('run_action', action_name)
 assert response is False
