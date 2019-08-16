628ae6e7890ca8955484c3ce04b2ac43c7893695
Wouter Glorieux <wouter@valyrian.tech>
2019-08-16 14:29:00 +0200

    python2/3 compatibility

--- integrationtests/integrationtest_action_Command.py
+++ integrationtests/integrationtest_action_Command.py
@@ -3,8 +3,8 @@
 from helpers.setupscripthelpers import spellbook_call, clean_up_actions
 
 
-print 'Starting Spellbook integration test: Command actions'
-print '----------------------------------------------\n'
+print('Starting Spellbook integration test: Command actions')
+print('----------------------------------------------\n')
 
 # Clean up actions if necessary
 clean_up_actions(action_ids=['integrationtest_action_Command'])
@@ -17,23 +17,23 @@ run_command = 'echo Hello world!'
 
 # --------------------------------------------------------------------------------------------------------
 
-print 'Creating test action: CommandAction'
+print('Creating test action: CommandAction')
 response = spellbook_call('save_action', '-t=Command', action_name, '-c=%s' % run_command)
 assert response is None
 
 # --------------------------------------------------------------------------------------------------------
-print 'Getting the list of configured action_ids'
+print('Getting the list of configured action_ids')
 response = spellbook_call('get_actions')
 assert action_name in response
 
 # --------------------------------------------------------------------------------------------------------
-print 'Getting the action config of the action we just created'
+print('Getting the action config of the action we just created')
 response = spellbook_call('get_action_config', action_name)
 assert response['id'] == action_name
 assert response['action_type'] == 'Command'
 assert response['run_command'] == run_command
 
 # --------------------------------------------------------------------------------------------------------
-print 'Running the action we just created'
+print('Running the action we just created')
 response = spellbook_call('run_action', action_name)
 assert response is True
