378a24eb4d48c8d488be57ec78070f713f21116c
Wouter Glorieux <wouter@valyrian.tech>
2019-08-16 13:58:06 +0200

    python2/3 compatibility

--- integrationtests/integration_test_trigger_conditions_HTTPDeleteRequestTrigger.py
+++ integrationtests/integration_test_trigger_conditions_HTTPDeleteRequestTrigger.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
 import requests
 import simplejson
 
 from helpers.setupscripthelpers import spellbook_call, clean_up_triggers
 from helpers.configurationhelpers import get_host, get_port
 
-print 'Starting Spellbook integration test: HTTP DELETE request trigger conditions'
-print '----------------------------------------------\n'
+from helpers.py2specials import *
+from helpers.py3specials import *
+
+print('Starting Spellbook integration test: HTTP DELETE request trigger conditions')
+print('----------------------------------------------\n')
 
 # Clean up triggers if necessary
 clean_up_triggers(trigger_ids=['test_trigger_conditions_HTTPDeleteRequest_Trigger'])
@@ -22,16 +24,16 @@ script = 'Echo.py'
 
 # -------------------------------------------------------------------------------------------------
 
-print 'Creating HTTP DELETE request trigger'
+print('Creating HTTP DELETE request trigger')
 response = spellbook_call('save_trigger', trigger_id, '-t=%s' % trigger_type, '-sc=%s' % script, '-st=Active', '--multi')
 assert response is None
 
-print 'Checking if trigger has not been triggered yet'
+print('Checking if trigger has not been triggered yet')
 response = spellbook_call('get_trigger_config', trigger_id)
 assert response['triggered'] is 0
 assert response['trigger_type'] == trigger_type
 
-print 'Activating HTTP DELETE request trigger with data'
+print('Activating HTTP DELETE request trigger with data')
 host, port = get_host(), get_port()
 url = 'http://{host}:{port}/spellbook/triggers/{trigger_id}/delete'.format(host=host, port=port, trigger_id=trigger_id)
 headers = {}
@@ -39,22 +41,22 @@ data = {'test': 'hello'}
 
 try:
     r = requests.delete(url, headers=headers, json=data)
-    print r.text
+    print(r.text)
     assert simplejson.loads(r.text) == data
 except Exception as ex:
-    print >> sys.stderr, 'DELETE %s failed: %s' % (url, ex)
+    print_to_stderr('DELETE %s failed: %s' % (url, ex))
     sys.exit(1)
 
 
-print 'Checking if trigger has been triggered'
+print('Checking if trigger has been triggered')
 response = spellbook_call('get_trigger_config', trigger_id)
 assert response['triggered'] > 0
 
-print 'Activating HTTP DELETE request trigger without data'
+print('Activating HTTP DELETE request trigger without data')
 try:
     r = requests.delete(url)
-    print r.text
+    print(r.text)
     assert r.text == ''
 except Exception as ex:
-    print >> sys.stderr, 'DELETE %s failed: %s' % (url, ex)
+    print_to_stderr('DELETE %s failed: %s' % (url, ex))
     sys.exit(1)
