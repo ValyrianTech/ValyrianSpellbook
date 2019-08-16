d94f79975d6b801da92ec2ade2b27c340852f9ad
Wouter Glorieux <wouter@valyrian.tech>
2019-08-16 14:49:37 +0200

    python2/3 compatibility
    Note: japanese mnemonics don't work under python3 for some reason

--- unittests/test_BIP39.py
+++ unittests/test_BIP39.py
@@ -3,7 +3,7 @@
 import pytest
 import binascii
 
-from BIP39_test_vectors import BIP39_test_vectors, BIP39_test_vectors_japanese
+from .BIP39_test_vectors import BIP39_test_vectors, BIP39_test_vectors_japanese
 from bips.BIP39 import get_seed
 
 testvectors = [[testvector[1], 'TREZOR', testvector[2]] for testvector in BIP39_test_vectors['english']]
@@ -12,23 +12,23 @@ testvectors_japanese = [[testvector['mnemonic'], testvector['passphrase'], testv
 
 class TestBIP39(object):
     def test_pytest(self):
-        print 'testing pytest...',
+        print('testing pytest...',)
         assert True
 
     @pytest.mark.parametrize('mnemonic, passphrase, seed', [testvector for testvector in testvectors])
     def test_mnemonic_to_seed(self, mnemonic, passphrase, seed):
-        print '\nMnemonic: %s' % mnemonic
-        print 'Passphrase: %s' % passphrase
-        print 'expected seed (hex): %s' % seed
-        print 'expected seed (bin): %s' % binascii.unhexlify(seed)
+        print('\nMnemonic: %s' % mnemonic)
+        print('Passphrase: %s' % passphrase)
+        print('expected seed (hex): %s' % seed)
+        print('expected seed (bin): %s' % binascii.unhexlify(seed))
 
         assert get_seed(mnemonic=mnemonic, passphrase=passphrase) == binascii.unhexlify(seed)
 
     @pytest.mark.parametrize('mnemonic, passphrase, seed', [testvector for testvector in testvectors_japanese])
     def test_mnemonic_to_seed_japanese(self, mnemonic, passphrase, seed):
-        print '\nMnemonic: %s' % mnemonic
-        print 'Passphrase: %s' % passphrase
-        print 'expected seed (hex): %s' % seed
-        print 'expected seed (bin): %s' % binascii.unhexlify(seed)
+        print('\nMnemonic: %s' % mnemonic)
+        print('Passphrase: %s' % passphrase)
+        print('expected seed (hex): %s' % seed)
+        print('expected seed (bin): %s' % binascii.unhexlify(seed))
 
         assert get_seed(mnemonic=mnemonic, passphrase=passphrase, language='japanese') == binascii.unhexlify(seed)
