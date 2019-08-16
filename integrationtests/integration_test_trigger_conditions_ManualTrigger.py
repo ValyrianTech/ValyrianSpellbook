af1fe9789d20a1ee1d50c0a354e59df4fdce2673
Wouter Glorieux <wouter@valyrian.tech>
2019-08-16 14:02:23 +0200

    python2/3 compatibility

--- integrationtests/integration_test_trigger_conditions_ManualTrigger.py
+++ integrationtests/integration_test_trigger_conditions_ManualTrigger.py
@@ -3,8 +3,8 @@
 from helpers.setupscripthelpers import spellbook_call, clean_up_triggers
 
 
-print 'Starting Spellbook integration test: Manual trigger conditions'
-print '----------------------------------------------\n'
+print('Starting Spellbook integration test: Manual trigger conditions')
+print('----------------------------------------------\n')
 
 # Clean up triggers if necessary
 clean_up_triggers(trigger_ids=['test_trigger_conditions_ManualTrigger'])
@@ -18,19 +18,19 @@ trigger_type = 'Manual'
 
 # -------------------------------------------------------------------------------------------------
 
-print 'Creating Manual trigger'
+print('Creating Manual trigger')
 response = spellbook_call('save_trigger', trigger_name, '-t=%s' % trigger_type, '-st=Active')
 assert response is None
 
-print 'Checking if trigger has not been triggered yet'
+print('Checking if trigger has not been triggered yet')
 response = spellbook_call('get_trigger_config', trigger_name)
 assert response['triggered'] == 0
 assert response['trigger_type'] == trigger_type
 
-print 'Activating Manual trigger'
+print('Activating Manual trigger')
 response = spellbook_call('activate_trigger', trigger_name)
 assert response is None
 
-print 'Checking if trigger has been triggered'
+print('Checking if trigger has been triggered')
 response = spellbook_call('get_trigger_config', trigger_name)
 assert response['triggered'] == 1
