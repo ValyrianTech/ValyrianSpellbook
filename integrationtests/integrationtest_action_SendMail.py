6a27a773fb17cb2a447612b7732ca335422786fa
Wouter Glorieux <wouter@valyrian.tech>
2019-08-16 14:32:57 +0200

    python2/3 compatibility

--- integrationtests/integrationtest_action_SendMail.py
+++ integrationtests/integrationtest_action_SendMail.py
@@ -3,8 +3,8 @@
 from helpers.setupscripthelpers import spellbook_call, clean_up_actions
 
 
-print 'Starting Spellbook integration test: SendMail actions'
-print '----------------------------------------------\n'
+print('Starting Spellbook integration test: SendMail actions')
+print('----------------------------------------------\n')
 
 # Clean up actions if necessary
 clean_up_actions(action_ids=['integrationtest_action_SendMail'])
@@ -15,21 +15,21 @@ clean_up_actions(action_ids=['integrationtest_action_SendMail'])
 action_name = 'integrationtest_action_SendMail'
 mail_recipients = 'someone@example.com'
 mail_subject = 'example email subject'
-mail_body_template = 'template1.txt'
+mail_body_template = 'template1'
 
 # --------------------------------------------------------------------------------------------------------
 
-print 'Creating test action: SendMailAction'
+print('Creating test action: SendMailAction')
 response = spellbook_call('save_action', '-t=SendMail', action_name, '-mr=%s' % mail_recipients, '-ms=%s' % mail_subject, "-mb=%s" % mail_body_template)
 assert response is None
 
 # --------------------------------------------------------------------------------------------------------
-print 'Getting the list of configured action_ids'
+print('Getting the list of configured action_ids')
 response = spellbook_call('get_actions')
 assert action_name in response
 
 # --------------------------------------------------------------------------------------------------------
-print 'Getting the action config of the action we just created'
+print('Getting the action config of the action we just created')
 response = spellbook_call('get_action_config', action_name)
 assert response['id'] == action_name
 assert response['action_type'] == 'SendMail'
@@ -38,6 +38,6 @@ assert response['mail_subject'] == mail_subject
 assert response['mail_body_template'] == mail_body_template
 
 # --------------------------------------------------------------------------------------------------------
-print 'Running the action we just created'
+print('Running the action we just created')
 response = spellbook_call('run_action', action_name)
 assert response is 'true' if mail_recipients != 'someone@example.com' else 'false'  # example.com is a reserved domain, so will always fail
