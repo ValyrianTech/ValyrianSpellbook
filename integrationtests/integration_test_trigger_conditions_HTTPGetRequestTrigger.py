f73f3acfaab48d3e3a715c323c20139838196e27
Wouter Glorieux <wouter@valyrian.tech>
2019-08-16 13:59:51 +0200

    python2/3 compatibility

--- integrationtests/integration_test_trigger_conditions_HTTPGetRequestTrigger.py
+++ integrationtests/integration_test_trigger_conditions_HTTPGetRequestTrigger.py
@@ -7,9 +7,11 @@ import simplejson
 from helpers.setupscripthelpers import spellbook_call, clean_up_triggers
 from helpers.configurationhelpers import get_host, get_port
 
+from helpers.py2specials import *
+from helpers.py3specials import *
 
-print 'Starting Spellbook integration test: HTTP GET request trigger conditions'
-print '----------------------------------------------\n'
+print('Starting Spellbook integration test: HTTP GET request trigger conditions')
+print('----------------------------------------------\n')
 
 # Clean up triggers if necessary
 clean_up_triggers(trigger_ids=['test_trigger_conditions_DeadMansSwitchTrigger'])
@@ -23,16 +25,16 @@ script = 'Echo.py'
 
 # -------------------------------------------------------------------------------------------------
 
-print 'Creating HTTP GET request trigger'
+print('Creating HTTP GET request trigger')
 response = spellbook_call('save_trigger', trigger_id, '-t=%s' % trigger_type, '-sc=%s' % script, '-st=Active', '--multi')
 assert response is None
 
-print 'Checking if trigger has not been triggered yet'
+print('Checking if trigger has not been triggered yet')
 response = spellbook_call('get_trigger_config', trigger_id)
 assert response['triggered'] is 0
 assert response['trigger_type'] == trigger_type
 
-print 'Activating HTTP GET request trigger with data'
+print('Activating HTTP GET request trigger with data')
 host, port = get_host(), get_port()
 url = 'http://{host}:{port}/spellbook/triggers/{trigger_id}/get'.format(host=host, port=port, trigger_id=trigger_id)
 headers = {}
@@ -40,22 +42,22 @@ data = {'test': 'hello'}
 
 try:
     r = requests.get(url, headers=headers, json=data)
-    print r.text
+    print(r.text)
     assert simplejson.loads(r.text) == data
 except Exception as ex:
-    print >> sys.stderr, 'GET %s failed: %s' % (url, ex)
+    print_to_stderr('GET %s failed: %s' % (url, ex))
     sys.exit(1)
 
 
-print 'Checking if trigger has been triggered'
+print('Checking if trigger has been triggered')
 response = spellbook_call('get_trigger_config', trigger_id)
 assert response['triggered'] > 0
 
-print 'Activating HTTP Get request trigger without data'
+print('Activating HTTP Get request trigger without data')
 try:
     r = requests.get(url)
-    print r.text
+    print(r.text)
     assert r.text == ''
 except Exception as ex:
-    print >> sys.stderr, 'GET %s failed: %s' % (url, ex)
+    print_to_stderr('GET %s failed: %s' % (url, ex))
     sys.exit(1)
