45128545d9ea92f7397e26ceda15845c7fe32560
Wouter Glorieux <wouter@valyrian.tech>
2019-08-17 16:24:13 +0200

    fix integrationtest recurring trigger

--- integrationtests/integration_test_trigger_conditions_RecurringTrigger.py
+++ integrationtests/integration_test_trigger_conditions_RecurringTrigger.py
@@ -42,28 +42,66 @@ assert response['begin_time'] == begin_time
 assert response['next_activation'] == begin_time
 assert response['triggered'] == 0
 
-print('Sleeping 2 seconds...')
-time.sleep(2)
-
-while int(time.time()) < end_time - (interval/2):
-    print('Checking recurring trigger each second')
-    response = spellbook_call('check_triggers', trigger_name)
-    assert response is None
-
-    response = spellbook_call('get_trigger_config', trigger_name)
-    assert response['trigger_type'] == trigger_type
-    assert response['triggered'] == 0
-
+print('Sleeping until begin time has been reached')
+while time.time() < begin_time:
     time.sleep(1)
 
-
-print('Sleeping until end time has passed')
-time.sleep(interval)
-
-print('Checking recurring trigger again, triggered should be true')
+print('Checking recurring trigger, should activate because begin time has been reached')
 response = spellbook_call('check_triggers', trigger_name)
 assert response is None
 
 response = spellbook_call('get_trigger_config', trigger_name)
 assert response['trigger_type'] == trigger_type
-assert response['triggered'] == 1  # todo check this
+assert response['begin_time'] == begin_time
+assert response['next_activation'] == begin_time + 10
+assert response['triggered'] == 1
+
+print('Checking recurring trigger again, should not activate because the next activation time has not been reached')
+response = spellbook_call('check_triggers', trigger_name)
+assert response is None
+
+response = spellbook_call('get_trigger_config', trigger_name)
+assert response['trigger_type'] == trigger_type
+assert response['begin_time'] == begin_time
+assert response['next_activation'] == begin_time + 10
+assert response['triggered'] == 1
+
+
+while int(time.time()) < end_time - 5:
+    n_triggered = 1 + (int(time.time()) - begin_time) // interval
+    print('Checking recurring trigger every few seconds')
+    response = spellbook_call('check_triggers', trigger_name)
+    assert response is None
+
+    response = spellbook_call('get_trigger_config', trigger_name)
+    assert response['trigger_type'] == trigger_type
+
+    print('Number of times triggered should be %s now' % n_triggered)
+    assert response['triggered'] == n_triggered
+
+    time.sleep(3)
+
+    response = spellbook_call('check_triggers', trigger_name)
+    assert response is None
+
+    response = spellbook_call('get_trigger_config', trigger_name)
+    assert response['trigger_type'] == trigger_type
+
+    print('Number of times triggered should be %s now' % n_triggered)
+    assert response['triggered'] == n_triggered
+
+    while time.time() < begin_time + n_triggered*interval:
+        time.sleep(0.01)
+
+
+print('Sleeping until end time has passed')
+while time.time() <= end_time:
+    time.sleep(1)
+
+print('Checking recurring trigger again, triggered should not have increased since last activation')
+response = spellbook_call('check_triggers', trigger_name)
+assert response is None
+
+response = spellbook_call('get_trigger_config', trigger_name)
+assert response['trigger_type'] == trigger_type
+assert response['triggered'] == (end_time - begin_time) // interval == n_triggered
