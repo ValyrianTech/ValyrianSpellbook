b639b37be75b6772600281c07edb16d1ed72db66
Wouter Glorieux <wouter@valyrian.tech>
2019-08-17 13:53:10 +0200

    python2/3 compatibility

--- apps/PaymentProcessor/check_payment_request_status.py
+++ apps/PaymentProcessor/check_payment_request_status.py
@@ -9,7 +9,7 @@ payment_request_id = input("Enter the payment-request-id")
 url = 'http://{host}:{port}/spellbook/triggers/PaymentProcessorPaymentStatus/get'.format(host=get_host(), port=get_port())
 data = {'payment_request_id': payment_request_id}
 
-print 'Checking status of Payment Request %s' % payment_request_id
+print('Checking status of Payment Request %s' % payment_request_id)
 try:
     r = requests.get(url, json=data)
     print(r.text)
