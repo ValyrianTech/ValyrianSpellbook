6f82207737707bdaaeb8b1c63b35339c5d5838dc
Wouter Glorieux <wouter@valyrian.tech>
2019-08-17 13:59:56 +0200

    python2/3 compatibility

--- apps/Voucher/make_new_voucher_request.py
+++ apps/Voucher/make_new_voucher_request.py
@@ -4,16 +4,20 @@ import requests
 from helpers.configurationhelpers import get_host, get_port
 from helpers.hotwallethelpers import get_address_from_wallet
 
+from helpers.py2specials import *
+from helpers.py3specials import *
+
+
 voucher = 'ABC123'
 address = get_address_from_wallet(account=0, index=1)
 
 url = 'http://{host}:{port}/api/RedeemVoucher'.format(host=get_host(), port=get_port())
 data = {'voucher': voucher, 'address': address}
 
-print 'Making new Voucher request'
+print('Making new Voucher request')
 try:
     r = requests.post(url, json=data)
-    print r.text
+    print(r.text)
 except Exception as ex:
-    print >> sys.stderr, 'POST %s failed: %s' % (url, ex)
+    print_to_stderr('POST %s failed: %s' % (url, ex))
     sys.exit(1)
