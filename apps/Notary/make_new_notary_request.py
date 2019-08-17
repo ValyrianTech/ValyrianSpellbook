607c7b32c43d2bcb062ff2e4362ca223abca9f9f
Wouter Glorieux <wouter@valyrian.tech>
2019-08-17 13:58:44 +0200

    python2/3 compatibility

--- apps/Notary/make_new_notary_request.py
+++ apps/Notary/make_new_notary_request.py
@@ -2,15 +2,16 @@ import sys
 import requests
 
 from helpers.configurationhelpers import get_host, get_port
-
+from helpers.py2specials import *
+from helpers.py3specials import *
 
 url = 'http://{host}:{port}/spellbook/triggers/Notary-request/post'.format(host=get_host(), port=get_port())
 data = {'message': 'This is a test message2'}
 
-print 'Making new Notary request'
+print('Making new Notary request')
 try:
     r = requests.post(url, json=data)
-    print r.text
+    print(r.text)
 except Exception as ex:
-    print >> sys.stderr, 'POST %s failed: %s' % (url, ex)
+    print_to_stderr('POST %s failed: %s' % (url, ex))
     sys.exit(1)
