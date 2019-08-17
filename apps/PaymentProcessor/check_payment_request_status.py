5a71307e7d9466b4aa9676f8505adbd53914861c
Wouter Glorieux <wouter@valyrian.tech>
2019-08-17 13:52:41 +0200

    python2/3 compatibility

--- apps/PaymentProcessor/check_payment_request_status.py
+++ apps/PaymentProcessor/check_payment_request_status.py
@@ -2,15 +2,17 @@ import sys
 import requests
 
 from helpers.configurationhelpers import get_host, get_port
+from helpers.py2specials import *
+from helpers.py3specials import *
 
-payment_request_id = raw_input("Enter the payment-request-id")
+payment_request_id = input("Enter the payment-request-id")
 url = 'http://{host}:{port}/spellbook/triggers/PaymentProcessorPaymentStatus/get'.format(host=get_host(), port=get_port())
 data = {'payment_request_id': payment_request_id}
 
 print 'Checking status of Payment Request %s' % payment_request_id
 try:
     r = requests.get(url, json=data)
-    print r.text
+    print(r.text)
 except Exception as ex:
-    print >> sys.stderr, 'GET %s failed: %s' % (url, ex)
+    print_to_stderr('GET %s failed: %s' % (url, ex))
     sys.exit(1)
