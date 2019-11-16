57618699c893dcc82622271afc2da5c8e56fff6f
Wouter Glorieux <wouter@valyrian.tech>
2019-11-16 14:00:40 +0100

    removed python2 support

--- apps/Notary/make_new_notary_request.py
+++ apps/Notary/make_new_notary_request.py
@@ -2,8 +2,6 @@ import sys
 import requests
 
 from helpers.configurationhelpers import get_host, get_port
-from helpers.py2specials import *
-from helpers.py3specials import *
 
 url = 'http://{host}:{port}/spellbook/triggers/Notary-request/post'.format(host=get_host(), port=get_port())
 data = {'message': 'This is a test message2'}
@@ -13,5 +11,5 @@ try:
     r = requests.post(url, json=data)
     print(r.text)
 except Exception as ex:
-    print_to_stderr('POST %s failed: %s' % (url, ex))
+    print('POST %s failed: %s' % (url, ex), file=sys.stderr)
     sys.exit(1)
