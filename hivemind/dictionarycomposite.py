67b832fbffa541d591ee244b63f2ed97d3ff50b8
Wouter Glorieux <wouter@valyrian.tech>
2019-11-16 14:24:50 +0100

    python3 compatibility

--- hivemind/dictionarycomposite.py
+++ hivemind/dictionarycomposite.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from helpers.loghelpers import LOG
-from compositehivemind import CompositeHivemind
-from hivemind import HivemindState
+from .compositehivemind import CompositeHivemind
+from .hivemind import HivemindState
 from helpers.hivemindhelpers import get_hivemind_state_hash
-
+from helpers.loghelpers import LOG
 
 class DictionaryComposite(CompositeHivemind):
     def get_result(self):
