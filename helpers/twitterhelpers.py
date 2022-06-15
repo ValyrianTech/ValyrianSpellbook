f4422f117dc0cfb46bccf028d4ec8267d7a72e32
Wouter Glorieux <wouter@valyrian.tech>
2022-06-16 01:48:21 +0200

    added post_tweet helper

--- helpers/twitterhelpers.py
+++ helpers/twitterhelpers.py
@@ -1,6 +1,11 @@
 import tweepy
 from helpers.configurationhelpers import get_twitter_consumer_key, get_twitter_consumer_secret, get_twitter_access_token, get_twitter_access_token_secret
 
+# For Twitter API to work, you need to enable developer portal on your twitter account
+# go to https://developer.twitter.com/
+# To post tweets and follow or unfollow, you also need to apply for elevated access to the API
+# Once Elevated Access is granted, also set the app permissions to read and write
+
 
 def get_twitter_api():
     """
@@ -23,3 +28,10 @@ def get_twitter_api():
         return
 
     return api
+
+
+def post_tweet(text):
+    api = get_twitter_api()
+
+    if api is not None:
+        api.update_status(status=text)
