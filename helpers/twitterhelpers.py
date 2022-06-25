f45afd4f4d9229c635c9d10c593696e5a2fbcb07
Wouter Glorieux <wouter@valyrian.tech>
2022-06-25 17:03:30 +0200

    added twitter helpers

--- helpers/twitterhelpers.py
+++ helpers/twitterhelpers.py
@@ -1,4 +1,6 @@
 import tweepy
+import random
+
 from helpers.configurationhelpers import get_twitter_consumer_key, get_twitter_consumer_secret, get_twitter_access_token, get_twitter_access_token_secret
 
 import time
@@ -33,8 +35,6 @@ def get_twitter_api():
 
 
 def post_tweet(text):
-    api = get_twitter_api()
-
     if api is not None:
         api.update_status(status=text)
 
@@ -43,7 +43,7 @@ def get_tweets(searchtext, limit=100):
     client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAAMSSdgEAAAAAvv5dDJ9usoBf0OELOGfTNWBvaSo%3DTcZhCoaeccrh3Ur2QzWqbMI2Jg23NZMRyiY60zKnn0X8VRMbWp")
 
     tweets = []
-    for i, tweet in enumerate(tweepy.Paginator(client.search_recent_tweets, searchtext, tweet_fields=['author_id', 'public_metrics'], user_fields=['public_metrics'], max_results=100).flatten(limit=limit)):
+    for i, tweet in enumerate(tweepy.Paginator(client.search_recent_tweets, searchtext, tweet_fields=['author_id', 'public_metrics', 'lang', 'attachments'], user_fields=['public_metrics'], max_results=100).flatten(limit=limit)):
         tweets.append(tweet)
 
     return tweets
@@ -57,3 +57,28 @@ def get_users(ids):
         return response.data
 
     return []
+
+
+def get_trending_topics(woeid=None):
+    # WOEID 1 = worldwide, see api.available_trends()
+    if woeid is None:
+        woeids = [trend['woeid'] for trend in api.available_trends()]
+        woeid = random.choice(woeids)
+
+    trends = api.get_place_trends(id=woeid)
+
+    topics = [trend['name'] for trend in trends[0]['trends']]
+
+    return topics
+
+
+def get_extended_status(status_id):
+    status = api.get_status(id=status_id, tweet_mode='extended')
+    return status
+
+
+def follow_user(username):
+    api.create_friendship(screen_name=username)
+
+
+api = get_twitter_api()
