b48de30c8e02f3a22b2f7942deca91165c17c83a
Wouter Glorieux <wouter@valyrian.tech>
2022-12-02 01:20:13 +0100

    twitter actions

---
+++ action/retweet_action.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from helpers.loghelpers import LOG
+from .action import Action
+from .actiontype import ActionType
+from helpers.twitterhelpers import retweet
+
+
+class RetweetAction(Action):
+    def __init__(self, action_id):
+        super(RetweetAction, self).__init__(action_id=action_id)
+        self.action_type = ActionType.RETWEET
+        self.tweet_id = None
+
+    def run(self):
+        """
+        Run the action
+
+        :return: True upon success, False upon failure
+        """
+        if self.tweet_id is None:
+            return False
+
+        LOG.info('Retweeting tweet: %s' % self.tweet_id)
+
+        try:
+            retweet(tweet_id=self.tweet_id)
+        except Exception as ex:
+            LOG.error(f'Unable to retweet tweet {self.tweet_id}: {ex}')
+            return False
+
+        return True
+
+    def configure(self, **config):
+        """
+        Configure the action with given config settings
+
+        :param config: A dict containing the configuration settings
+                       - config['tweet_id']    : The id of the tweet to retweet
+        """
+        super(RetweetAction, self).configure(**config)
+        if 'tweet_id' in config:
+            self.tweet_id = config['tweet_id']
+
+    def json_encodable(self):
+        """
+        Get the action config in a json encodable format
+
+        :return: A dict containing the configuration settings
+        """
+        ret = super(RetweetAction, self).json_encodable()
+        ret.update({'tweet_id': self.tweet_id})
+        return ret
