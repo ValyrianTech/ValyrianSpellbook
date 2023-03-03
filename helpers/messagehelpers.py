d6927c28fe5b19461c0e3323e83463eb5629d0b9
Wouter Glorieux <wouter@valyrian.tech>
2023-03-03 21:12:30 +0100

    tmp

--- helpers/messagehelpers.py
+++ helpers/messagehelpers.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import base64
+from bitcoin.core.key import CPubKey
+from bitcoin.wallet import P2PKHBitcoinAddress
 
 import bitcoin
 from bitcoin.wallet import CBitcoinSecret
@@ -19,6 +22,20 @@ def verify_message(address, message, signature):
     print(f'Verifying message: {message} with signature: {signature} for address: {address}')
     tmp = BitcoinMessage(message)
     print(f'BitcoinMessage: {tmp.GetHash()}')
+
+    sig = base64.b64decode(signature)
+    print(f'sig: {sig}')
+    hash = tmp.GetHash()
+    print(f'hash: {hash}')
+
+    pubkey = CPubKey.recover_compact(hash, sig)
+    print(f'pubkey: {pubkey}')
+    print(f'{str(P2PKHBitcoinAddress.from_pubkey(pubkey))}')
+    print(f'{str(address)}')
+
+    print(f'Result: {str(P2PKHBitcoinAddress.from_pubkey(pubkey)) == str(address)}')
+
+
     try:
         return VerifyMessage(address=address, message=BitcoinMessage(message), sig=signature)
     except Exception as ex:
