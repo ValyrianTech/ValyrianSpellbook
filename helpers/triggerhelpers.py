8e3d1b2f1dc29a65203980468d9236c3d12928b5
Wouter Glorieux <wouter@valyrian.tech>
2023-03-05 03:33:30 +0100

    added sign_data to messagehelpers added signed_message_request to triggerhelpers

--- helpers/triggerhelpers.py
+++ helpers/triggerhelpers.py
@@ -325,3 +325,28 @@ def http_delete_request(trigger_id, **data):
         if len(data) > 0:
             trigger.set_json_data(data=data)
         return trigger.activate()
+
+
+def signed_message_request(trigger_id, **data):
+    triggers = get_triggers()
+    if trigger_id not in triggers:
+        return {'error': 'Unknown trigger id: %s' % trigger_id}
+
+    trigger = get_trigger(trigger_id)
+    if trigger.trigger_type != TriggerType.SIGNEDMESSAGE:
+        return {'error': 'Trigger %s is not a SignedMessage request trigger but a %s trigger' % (trigger_id, trigger.trigger_type)}
+
+    if trigger.status == 'Active':
+        LOG.info('Trigger %s received a SignedMessage request' % trigger_id)
+
+        if 'message' in data:
+            trigger.message = data['message']
+        if 'message_address' in data:
+            trigger.message_address = data['message_address']
+        if 'message_signature' in data:
+            trigger.message_signature = data['message_signature']
+
+        if len(data) > 0:
+            trigger.set_json_data(data=data)
+
+        return trigger.activate()
