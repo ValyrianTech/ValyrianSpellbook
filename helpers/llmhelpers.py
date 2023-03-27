b231d66ec176c220f1bca693d923bcd95571c7e4
Wouter Glorieux <wouter@valyrian.tech>
2023-03-27 02:20:08 +0200

    default model_name

--- helpers/llmhelpers.py
+++ helpers/llmhelpers.py
@@ -6,7 +6,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, AIMessage, SystemMessage, ChatMessage, BaseMessage
 
 
-def get_llm(model_name: str, temperature: float = 0.0):
+def get_llm(model_name: str = 'gpt-3.5-turbo', temperature: float = 0.0):
     if get_enable_openai() is True:
         if model_name == 'text-davinci-003':
             llm = OpenAI(model_name=model_name, temperature=temperature, openai_api_key=get_openai_api_key())
