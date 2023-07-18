84eeaa7a2c29ed5c64d24227e380f5f38c6ab814
Wouter Glorieux <wouter@valyrian.tech>
2023-07-18 18:03:01 +0200

    set openssl path instead of using nix-shell

--- start_spellbook.sh
+++ start_spellbook.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sleep 10
 source /spellbook/venv/bin/activate
-nix-shell -p openssl
+OPENSSL_BIN=${pkgs.openssl}/bin/openssl
+export PATH="$OPENSSL_BIN:$PATH"
 python /spellbook/spellbookserver.py
