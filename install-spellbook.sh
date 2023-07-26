d87c23e387f55bd139de0d5bcdaed75c77619208
Wouter Glorieux <wouter@valyrian.tech>
2023-07-26 15:09:43 +0200

    updated install script

--- install-spellbook.sh
+++ install-spellbook.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# run this script on a fresh linux machine to install the Bitcoin Spellbook and all requirements
-# this script will install The Bitcoin Spellbook as root and create all directories at the root of the system
+# run this script on a fresh linux machine to install the Valyrian Spellbook and all requirements
+# this script will install The Valyrian Spellbook as root and create all directories at the root of the system
 
 # cd /
-# wget https://raw.githubusercontent.com/ValyrianTech/BitcoinSpellbook/master/install-spellbook.sh
+# wget https://raw.githubusercontent.com/ValyrianTech/ValyrianSpellbook/master/install-spellbook.sh
 # sh install-spellbook.sh
 
 cd /
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install git python3.10 python3.10-dev python3-pip -y
+sudo apt-get install libssl-dev
 
 mkdir spellbook_wallet
 mkdir spellbook_data
 
-git clone https://github.com/ValyrianTech/BitcoinSpellbook.git spellbook
+git clone https://github.com/ValyrianTech/ValyrianSpellbook.git spellbook
 cd /spellbook
 
 python3.10 -m pip install -r requirements.txt
