707fb285eca6ca0423fac97f39b0a34582a57c15
Wouter Glorieux <wouter@valyrian.tech>
2023-09-21 17:12:22 +0200

    allowing for separate channels to subscribe and unsubscribe in the websocket

--- helpers/websockethelpers.py
+++ helpers/websockethelpers.py
@@ -8,6 +8,7 @@ from loghelpers import LOG
 class WebSocketHandler:
     def __init__(self):
         self.connected = set()
+        self.subscriptions = {}
         self.lock = asyncio.Lock()
 
     async def handler(self, websocket, path):
@@ -15,32 +16,41 @@ class WebSocketHandler:
             # Register.
             async with self.lock:
                 self.connected.add(websocket)
+                self.subscriptions[websocket] = {'general'}
             LOG.debug(f'New connection: {websocket.remote_address}')
             # Implement logic here.
             async for message in websocket:
-                await self.broadcast(message)
+                if message.startswith('subscribe:'):
+                    async with self.lock:
+                        self.subscriptions[websocket].add(message.split(':')[1])
+                elif message.startswith('unsubscribe:'):
+                    async with self.lock:
+                        self.subscriptions[websocket].remove(message.split(':')[1])
+                else:
+                    await self.broadcast(message, 'general')
         except Exception as e:
             LOG.error(f'Error in handler: {e}')
         finally:
             # Unregister.
             async with self.lock:
                 self.connected.remove(websocket)
+                del self.subscriptions[websocket]
             await websocket.close()
             LOG.debug(f'Connection closed: {websocket.remote_address}')
 
-    async def broadcast(self, message):
+    async def broadcast(self, message, channel='general'):
         LOG.info(f'Broadcasting {message}')
         async with self.lock:
             if self.connected:  # asyncio.wait doesn't accept an empty list
-                await asyncio.wait([user.send(message) for user in self.connected])
+                await asyncio.wait([user.send(message) for user in self.connected if channel in self.subscriptions[user]])
 
 
 WEBSOCKET_HANDLER = WebSocketHandler()
 LOOP = asyncio.new_event_loop()
 
 
-def broadcast_message(message: str):
-    asyncio.run_coroutine_threadsafe(WEBSOCKET_HANDLER.broadcast(message), LOOP)
+def broadcast_message(message: str, channel: str = 'general'):
+    asyncio.run_coroutine_threadsafe(WEBSOCKET_HANDLER.broadcast(message, channel), LOOP)
 
 
 def start_websocket_server(host: str, port: int):
