b0d21bff83c425569faecf69a66ac7aec8221eb2
Wouter Glorieux <wouter@valyrian.tech>
2024-09-18 22:57:59 +0200

    Add fallback mechanism to retry if load fails

--- helpers/jsonhelpers.py
+++ helpers/jsonhelpers.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
+
 import simplejson
 
 from helpers.loghelpers import LOG
@@ -38,5 +40,12 @@ def load_from_json_file(filename):
             data = simplejson.load(input_file)
         except Exception as ex:
             LOG.error('Failed to load %s: %s' % (filename, ex))
+            LOG.error('Sleeping for 1 second before retrying')
+            time.sleep(1)
+            LOG.error('Retrying to load %s' % filename)
+            try:
+                data = simplejson.load(input_file)
+            except Exception as ex:
+                LOG.error('Failed to load twice %s: %s' % (filename, ex))
 
     return data
